72a8e23e02cd614398c0ef71f00ee8dfc9b4e39a
aron-tw <aron886tw@gmail.com>
2021-12-15 01:40:22 +0800

    fix: height

--- src/height.ts
+++ src/height.ts
@@ -1,10 +1,13 @@
-import { HEIGHT } from './constants/css-property-keyword';
+import { FIT, FIT_CONTENT, FULL, HEIGHT, H_PREFIX, MAX, MAX_CONTENT, MIN, MIN_CONTENT, PERCENT100 } from './constants/css-property-keyword';
 import { MasterStyle } from '@master/style';
 
 export class HeightStyle extends MasterStyle {
-    static override prefixes =  /^h:/;
+    static override prefixes = /^h:/;
     static override properties = [HEIGHT];
     static override semantics = {
-        'h:full': '100%'
+        [H_PREFIX + FULL]: PERCENT100,
+        [H_PREFIX + FIT]: FIT_CONTENT,
+        [H_PREFIX + MAX]: MAX_CONTENT,
+        [H_PREFIX + MIN]: MIN_CONTENT
     }
 }
--- src/variable.ts
+++ src/variable.ts
@@ -2,6 +2,7 @@ import { MasterStyle } from '@master/style';
 
 export class VariableStyle extends MasterStyle {
     static override prefixes = /^\$.*:/;
+    static override defaultUnit = ''; // don't use 'rem' as default, because css variable is common API
     override get properties(): { [key: string]: any } {
         return {
             ['--' + this.prefix.slice(1, -1)]: this
