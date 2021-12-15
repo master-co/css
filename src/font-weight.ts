857bc7b13e09739ebd20d12fd7aa61b9108e9bd6
aron-tw <aron886tw@gmail.com>
2021-12-16 01:29:34 +0800

    refactor: font-weight

--- src/font-weight.ts
+++ src/font-weight.ts
@@ -2,18 +2,23 @@ import { DASH, FONT, F_PREFIX, WEIGHT } from './constants/css-property-keyword';
 import { MasterStyle } from '@master/style';
 
 export class FontWeightStyle extends MasterStyle {
-    static override prefixes =  /^f-weight:/;
+    static override prefixes = /^f-weight:/;
     static override properties = [FONT + DASH + WEIGHT];
     static override defaultUnit = '';
-    static override semantics = {
-        [F_PREFIX + 'thin']: 100,
-        [F_PREFIX + 'extralight']: 200,
-        [F_PREFIX + 'light']: 300,
-        [F_PREFIX + 'regular']: 400,
-        [F_PREFIX + 'medium']: 500,
-        [F_PREFIX + 'semibold']: 600,
-        [F_PREFIX + 'bold']: 700,
-        [F_PREFIX + 'extrabold']: 800,
-        [F_PREFIX + 'heavy']: 900
-    }
+}
+
+let weight = 100;
+for (const name of [
+    'thin',
+    'extralight',
+    'light',
+    'regular',
+    'medium',
+    'semibold',
+    'bold',
+    'extrabold',
+    'heavy'
+]) {
+    FontWeightStyle.values[name] = weight;
+    weight += 100;
 }
