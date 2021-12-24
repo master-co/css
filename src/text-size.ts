8e07f315922953159514a3ea27695dc5aa834dc5
aron-tw <aron886tw@gmail.com>
2021-12-24 21:06:26 +0800

    remove const

--- src/text-size.ts
+++ src/text-size.ts
@@ -1,12 +1,12 @@
-import { DASH, FONT, HEIGHT, LINE, REM, SIZE } from './constants/css-property-keyword';
+import { REM } from './constants/css-property-keyword';
 import { Style } from '@master/style';
 
 export class TextSizeStyle extends Style {
     static override prefixes = /^(t(ext)?:[0-9]((?!;).)*$)/;
     override get props(): { [key: string]: any } {
         return {
-            [FONT + DASH + SIZE]: this,
-            [LINE + DASH + HEIGHT]: {
+            'font-size': this,
+            'line-height': {
                 ...this,
                 value: this.unit === REM
                     ? this.value + .375 + this.unit
