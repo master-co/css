02c3112ed193c2ed28b84916ce6f6d6cb2d8e45d
aron-tw <aron886tw@gmail.com>
2021-12-26 23:50:17 +0800

    fix: font family

--- src/font-family.ts
+++ src/font-family.ts
@@ -1,14 +1,14 @@
 import { DASH, FAMILY, FONT, MONO, SANS, SERIF, VAR_END, VAR_START } from './constants/css-property-keyword';
 import { Style } from '@master/style';
 
-const VAR_F = VAR_START + 'f' + DASH;
+const VAR_FONT = VAR_START + 'font' + DASH;
 
 export class FontFamilyStyle extends Style {
     static override matches = /^f-family:.|f(ont)?:(mono|sans|serif)/;
     static override key = FONT + DASH + FAMILY;
     static override values = {
-        mono: VAR_F + MONO + VAR_END,
-        sans: VAR_F + SANS + VAR_END,
-        serif: VAR_F + SERIF + VAR_END
+        mono: VAR_FONT + MONO + VAR_END,
+        sans: VAR_FONT + SANS + VAR_END,
+        serif: VAR_FONT + SERIF + VAR_END
     }
 }
