468cec56bb938ce189704bd7328f4b751d9ec4b7
BenSeage <bg012300@gmail.com>
2021-12-30 13:54:59 +0800

    feat: columns

---
+++ src/columns.ts
@@ -0,0 +1,6 @@
+import { Style } from '@master/style';
+import { COLUMNS } from './constants/css-property-keyword';
+
+export class ColumnsStyle extends Style {
+    static override key = COLUMNS;
+}
