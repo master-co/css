d5dbce7327553e47fc28b9f053d7d17b6f3a326c
aron-tw <aron886tw@gmail.com>
2022-01-02 22:07:22 +0800

    fix: pointer-events

--- src/pointer-events.ts
+++ src/pointer-events.ts
@@ -1,6 +1,6 @@
 import { Style } from '@master/style';
-import { DASH, EVENTS, POINTERS } from './constants/css-property-keyword';
+import { DASH, EVENTS, POINTER } from './constants/css-property-keyword';
 
 export class PointerEventsStyle extends Style {
-    static override key = POINTERS + DASH + EVENTS;
+    static override key = POINTER + DASH + EVENTS;
 }
