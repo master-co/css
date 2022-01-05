deb4c40cd5c7ce7499f7c93603085ef7b3b6f83d
aron-tw <aron886tw@gmail.com>
2022-01-05 23:03:25 +0800

    fix: remove border-spacing prefix b-

--- src/border-spacing.ts
+++ src/border-spacing.ts
@@ -2,6 +2,5 @@ import { Style } from '@master/style';
 import { BORDER, DASH, SPACING } from './constants/css-property-keyword';
 
 export class BorderSpacingStyle extends Style {
-    static override matches = /^b-spacing:./;
     static override key = BORDER + DASH + SPACING;
 }
