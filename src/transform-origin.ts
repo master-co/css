f1248df7ba09dc476c1260497189810f4fd6e8fc
BenSeage <bg012300@gmail.com>
2022-01-06 17:08:51 +0800

    fix: transform-origin

--- src/transform-origin.ts
+++ src/transform-origin.ts
@@ -1,7 +1,8 @@
-import { DASH, ORIGIN, TRANSFORM } from './constants/css-property-keyword';
+import { DASH, ORIGIN, PX, TRANSFORM } from './constants/css-property-keyword';
 import { Style } from '@master/style';
 
 export class TransformOriginStyle extends Style {
-    static override matches =  /^transform:(top|bottom|right|left|center)(?!;)/;
+    static override matches =  /^transform:((top|bottom|right|left|center)|\d)/;
     static override key = TRANSFORM + DASH + ORIGIN;
+    static override unit = PX;
 }
