8b8170fc5da7643e55a294f2959dd0356e1a3972
BenSeage <bg012300@gmail.com>
2022-01-06 17:27:17 +0800

    fix: scroll-snap-stop

--- src/scroll-snap-stop.ts
+++ src/scroll-snap-stop.ts
@@ -2,5 +2,6 @@ import { Style } from '@master/style';
 import { DASH, SCROLL, SNAP, STOP } from './constants/css-property-keyword';
 
 export class ScrollSnapStopStyle extends Style {
+    static override matches = /^scroll-snap:(normal|always)(?!;)/
     static override key = SCROLL + DASH + SNAP + DASH + STOP;
 }
