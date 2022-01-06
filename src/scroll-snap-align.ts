929322b11036e93490ac281819ae2aa26e04321d
BenSeage <bg012300@gmail.com>
2022-01-06 17:26:02 +0800

    fix: scroll-snap-align

--- src/scroll-snap-align.ts
+++ src/scroll-snap-align.ts
@@ -2,5 +2,6 @@ import { Style } from '@master/style';
 import { ALIGN, DASH, SCROLL, SNAP } from './constants/css-property-keyword';
 
 export class ScrollSnapAlignStyle extends Style {
+    static override matches = /^scroll-snap:(start|end|center)/
     static override key = SCROLL + DASH + SNAP + DASH + ALIGN;
 }
