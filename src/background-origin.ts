a0a223238540359456599c398910263136d343af
BenSeage <bg012300@gmail.com>
2022-01-06 17:03:01 +0800

    fix: background-origin

--- src/background-origin.ts
+++ src/background-origin.ts
@@ -1,6 +1,12 @@
 import { Style } from '@master/style';
-import { BACKGROUND, DASH, ORIGIN } from './constants/css-property-keyword';
+import { BACKGROUND, BORDER, BOX, CONTENT, DASH, ORIGIN, PADDING } from './constants/css-property-keyword';
 
 export class BackgroundOriginStyle extends Style {
+    static override matches = /^(bg|background):(content|border|padding)(?!;)/;
     static override key = BACKGROUND + DASH + ORIGIN;
+    static override values = {
+        content: CONTENT + DASH + BOX,
+        border: BORDER + DASH + BOX,
+        padding: PADDING + DASH + BOX
+    }
 }
