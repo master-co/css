9068fb932b8a295b5bc6fee963f4a9a95594f19f
BenSeage <bg012300@gmail.com>
2022-01-07 17:28:34 +0800

    fix: matches

--- src/background-position.ts
+++ src/background-position.ts
@@ -2,7 +2,7 @@ import { Style } from '@master/style';
 import { BACKGROUND, DASH, POSITION, PX } from './constants/css-property-keyword';
 
 export class BackgroundPositionStyle extends Style {
-    static override matches = /^(bg|background):((top|bottom|right|left|center)|\d)/;
+    static override matches = /^(bg|background):((top|bottom|right|left|center)(?!;)|\d((?!;).)*$)/;
     static override key = BACKGROUND + DASH + POSITION;
     static override unit = PX;
 }
--- src/text-decoration-thickness.ts
+++ src/text-decoration-thickness.ts
@@ -2,7 +2,7 @@ import { DASH, DECORATION, TEXT, THICKNESS } from './constants/css-property-keyw
 import { Style } from '@master/style';
 
 export class TextDecorationThicknessStyle extends Style {
-    static override matches =  /^text-decoration:[0-9]((?!;).)*/;
+    static override matches =  /^text-decoration:[0-9]((?!;).)*$/;
     static override key = TEXT + DASH + DECORATION + DASH + THICKNESS;
     static override unit = 'em';
 }
