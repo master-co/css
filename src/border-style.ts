e3f62a717af551a838918245267676c68ba535ef
BenSeage <bg012300@gmail.com>
2022-01-08 16:18:16 +0800

    fix: border-width / border-style matches

--- src/border-style.ts
+++ src/border-style.ts
@@ -3,7 +3,7 @@ import { STYLE } from './constants/css-property-keyword';
 import { getBorderProps } from './utils/get-border-props';
 
 export class BorderStyleStyle extends Style {
-    static override matches = /^(border-style:.|b((x|y|t|b|l|r|order)|order-(left|right|top|bottom)-style)?:[0-9]((?!;).)*$)/;
+    static override matches = /^(border(-(left|right|top|bottom))?-style:.|b(x|y|t|b|l|r|order)?:(none|hidden|dotted|dashed|solid|double|groove|ridge|inset|outset)(?!;))/;
     override get props(): { [key: string]: any } {
         return getBorderProps(this.prefix, this, STYLE);
     }
