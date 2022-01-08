fb75bdaada6e449aafecd0ebebe5989804771cbb
aron-tw <aron886tw@gmail.com>
2022-01-08 15:22:34 +0800

    fix: background-clip

--- src/background-clip.ts
+++ src/background-clip.ts
@@ -1,7 +1,7 @@
 import { Style } from '@master/style';
 
 export class BackgroundClipStyle extends Style {
-    static override matches = /^background-clip:.|(bg|background):(content|border|padding|text)(?!;)/;
+    static override matches = /^background-clip:.|(bg|background):text(?!;)/;
     override get props(): { [key: string]: any } {
         return {
             '-webkit-background-clip': this,
