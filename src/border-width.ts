b95ef962bd643fd9ef764c9c73fa4b7951d35e20
BenSeage <bg012300@gmail.com>
2022-01-08 15:43:52 +0800

    fix: remove console

--- src/border-width.ts
+++ src/border-width.ts
@@ -5,7 +5,6 @@ import { getBorderProps } from './utils/get-border-props';
 export class BorderWidthStyle extends Style {
     static override matches = /^b((x|y|t|b|l|r|order)|order(-(left|right|top|bottom))?-width)?:[0-9]((?!;).)*$/;
     override get props(): { [key: string]: any } {
-        console.log(getBorderProps(this.prefix, this, WIDTH));
         return getBorderProps(this.prefix, this, WIDTH);
     }
 
