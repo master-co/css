f9fceaf0bad715595ec4f9741fc1dd4ca6338cb5
BenSeage <bg012300@gmail.com>
2022-01-09 21:04:04 +0800

    fix: background-repeat matches

--- src/background-repeat.ts
+++ src/background-repeat.ts
@@ -2,6 +2,6 @@ import { Style } from '@master/style';
 import { BACKGROUND, DASH, REPEAT } from './constants/css-property-keyword';
 
 export class BackgroundRepeatStyle extends Style {
-    static override matches = /^(bg|background):(repeat|no-repeat|repeat-x|repeat-y)(?!;)/;
+    static override matches = /^(bg|background):(space|round|repeat|no-repeat|repeat-x|repeat-y)(?!;)/;
     static override key = BACKGROUND + DASH + REPEAT;
 }
