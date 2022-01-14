e17142488019689bf67ce32182d147c863bad62c
BenSeage <bg012300@gmail.com>
2022-01-14 15:09:59 +0800

    fix: remove text-underline-offset matches

--- src/text-underline-offset.ts
+++ src/text-underline-offset.ts
@@ -2,6 +2,5 @@ import { DASH, OFFSET, TEXT, UNDERLINE } from './constants/css-property-keyword'
 import { Style } from '@master/style';
 
 export class TextUnderlineOffsetStyle extends Style {
-    static override matches =  /^text-underline-offset:./;
     static override key = TEXT + DASH + UNDERLINE + DASH + OFFSET;
 }
