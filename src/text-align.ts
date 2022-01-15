eb042d074f116b87d25683ec9ab28cdb1806ffcb
BenSeage <bg012300@gmail.com>
2022-01-15 17:41:31 +0800

    text-align

--- src/text-align.ts
+++ src/text-align.ts
@@ -2,6 +2,6 @@ import { ALIGN, DASH, TEXT } from './constants/css-property-keyword';
 import { Style } from '@master/style';
 
 export class TextAlignStyle extends Style {
-    static override matches =  /^t(ext)?:(justify|center|left|right)(?!;)/;
+    static override matches =  /^t(ext)?:(justify|center|left|right|start|end)(?!;)/;
     static override key = TEXT + DASH + ALIGN;
 }
