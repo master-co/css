03aa69a96b09d82731ba2502eec643de5cf4d306
BenSeage <bg012300@gmail.com>
2022-01-15 16:29:13 +0800

    fix: remove text-decoration blink matches

--- src/text-decoration-line.ts
+++ src/text-decoration-line.ts
@@ -2,6 +2,6 @@ import { DASH, DECORATION, LINE, TEXT } from './constants/css-property-keyword';
 import { Style } from '@master/style';
 
 export class TextDecorationLineStyle extends Style {
-    static override matches =  /^t(ext)?:(none|underline|overline|line-through|blink)(?!;)/;
+    static override matches =  /^t(ext)?:(none|underline|overline|line-through)(?!;)/;
     static override key = TEXT + DASH + DECORATION + DASH + LINE;
 }
