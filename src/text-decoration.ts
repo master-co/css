9df4eccd56aa37961e567127182d0ea6752beb6f
BenSeage <bg012300@gmail.com>
2022-01-15 16:19:08 +0800

    fix: text-decoration matches

--- src/text-decoration.ts
+++ src/text-decoration.ts
@@ -2,6 +2,6 @@ import { DASH, DECORATION, TEXT } from './constants/css-property-keyword';
 import { Style } from '@master/style';
 
 export class TextDecorationStyle extends Style {
-    static override matches =  /^t(ext)?:(underline|line-throught)/;
+    static override matches =  /^t(ext)?:(underline|line-through|overline|blink)/;
     static override key = TEXT + DASH + DECORATION;
 }
