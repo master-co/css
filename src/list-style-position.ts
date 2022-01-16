1c7a71ddb0e57b7eb8563f55a1b53b518f8087d0
BenSeage <bg012300@gmail.com>
2022-01-16 19:19:51 +0800

    fix: list-style-position matches

--- src/list-style-position.ts
+++ src/list-style-position.ts
@@ -2,5 +2,6 @@ import { DASH, LIST, POSITION, STYLE } from './constants/css-property-keyword';
 import { Style } from '@master/style';
 
 export class ListStylePositionStyle extends Style {
+    static override matches = /^list-style:(inside|outside)(?!;)/;
     static override key = LIST + DASH + STYLE + DASH + POSITION;
 }
