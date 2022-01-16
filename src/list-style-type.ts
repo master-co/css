f0755a0fbf6fb99ace72109d50dad496499ed0ee
BenSeage <bg012300@gmail.com>
2022-01-16 17:36:53 +0800

    fix: list-style-type matches

--- src/list-style-type.ts
+++ src/list-style-type.ts
@@ -2,5 +2,6 @@ import { DASH, LIST, STYLE, TYPE } from './constants/css-property-keyword';
 import { Style } from '@master/style';
 
 export class ListStyleTypeStyle extends Style {
+    static override matches = /^list-style:(none|disc|decimal)(?!;)/;
     static override key = LIST + DASH + STYLE + DASH + TYPE;
 }
