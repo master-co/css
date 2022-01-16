35f8faec54e2a166ab9171706da331720be88c7a
BenSeage <bg012300@gmail.com>
2022-01-16 16:39:56 +0800

    fix: grid-column-start matches

--- src/grid-column-start.ts
+++ src/grid-column-start.ts
@@ -2,6 +2,7 @@ import { COLUMN, DASH, GRID } from './constants/css-property-keyword';
 import { Style } from '@master/style';
 
 export class GridColumnStartStyle extends Style {
+    static override matches = /^grid-col-start:./;
     static override key = GRID + DASH + COLUMN + DASH + 'start';
     static override unit = '';
 }
