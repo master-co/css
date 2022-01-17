48c3ca2bd83c50a23d2d5ab4c0d6ca89bf4f1f62
BenSeage <bg012300@gmail.com>
2022-01-17 17:12:53 +0800

    fix: font matches

--- src/font.ts
+++ src/font.ts
@@ -2,6 +2,7 @@ import { FONT } from './constants/css-property-keyword';
 import { Style } from '@master/style';
 
 export class FontStyle extends Style {
+    static override matches = /^f:./;
     static override key = FONT;
     static override unit = '';
 }
