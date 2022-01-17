5f58b48b26bc59a0ff16fa14091fc3a38c212fd4
BenSeage <bg012300@gmail.com>
2022-01-17 15:31:54 +0800

    fix: shape-outside matches

--- src/shape-outside.ts
+++ src/shape-outside.ts
@@ -2,7 +2,7 @@ import { Style } from '@master/style';
 import { BORDER, BOX, CONTENT, DASH, MARGIN, PADDING, SHAPE } from './constants/css-property-keyword';
 
 export class ShapeOutsideStyle extends Style {
-    static override matches = /^shape:((margin|content|border|padding)(?!;)|(inset|circle|ellipse|polygon|url|linear-gradient)\(.*\)((?!;).)*)$/
+    static override matches = /^shape:((margin|content|border|padding)(?!;)|(inset|circle|ellipse|polygon|url|linear-gradient)\(.*\)((?!;).)*$)/
     static override key = SHAPE + DASH + 'outside';
     static override values = {
         content: CONTENT + DASH + BOX,
