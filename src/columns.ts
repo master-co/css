ad8701dc17d12f8a6377286e6f800a76811eb8b8
aron-tw <aron886tw@gmail.com>
2022-01-23 20:59:24 +0800

    fix: columns

--- src/columns.ts
+++ src/columns.ts
@@ -4,4 +4,5 @@ import { COLUMNS } from './constants/css-property-keyword';
 export class ColumnsStyle extends Style {
     static override matches = /^(columns|cols):./;
     static override key = COLUMNS;
+    static override unit = '';
 }
