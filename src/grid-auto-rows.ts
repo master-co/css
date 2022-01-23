abd5dc1e77d59cec699908124bcfbb898c6c5a37
BenSeage <bg012300@gmail.com>
2022-01-23 17:26:09 +0800

    fix: grid-auto-rows values

--- src/grid-auto-rows.ts
+++ src/grid-auto-rows.ts
@@ -1,7 +1,10 @@
-import { AUTO, DASH, GRID, ROWS } from './constants/css-property-keyword';
+import { AUTO, CONTENT, DASH, GRID, MAX, MIN, ROWS } from './constants/css-property-keyword';
 import { Style } from '@master/style';
 
 export class GridAutoRowsStyle extends Style {
     static override key = GRID + DASH + AUTO + DASH + ROWS;
-
+    static override values = {
+        'min': MIN + DASH + CONTENT,
+        'max': MAX + DASH + CONTENT
+    };
 }
