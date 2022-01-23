f6592ce5117b37c4bf8c9ab8d05761eb8e1a63ba
BenSeage <bg012300@gmail.com>
2022-01-23 14:45:41 +0800

    fix: box-decoration-break

--- src/box-decoration-break.ts
+++ src/box-decoration-break.ts
@@ -1,9 +1,7 @@
 import { Style } from '@master/style';
-import { BOX, BREAK, DASH, DECORATION } from './constants/css-property-keyword';
 
 export class BoxDecorationBreakStyle extends Style {
     static override matches = /^box:(slice|clone)(?!;)/;
-    static override key = BOX + DASH + DECORATION + DASH + BREAK;
     override get props(): { [key: string]: any } {
         return {
             'box-decoration-break': this,
