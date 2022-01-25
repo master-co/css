4579b763f29ee036ecfc5e2601502c642d96302f
BenSeage <bg012300@gmail.com>
2022-01-26 00:12:52 +0800

    fix: grid-template-area

--- src/grid-template-areas.ts
+++ src/grid-template-areas.ts
@@ -1,6 +1,6 @@
-import { DASH, GRID, TEMPLATE } from './constants/css-property-keyword';
+import { AREA, DASH, GRID, TEMPLATE } from './constants/css-property-keyword';
 import { Style } from '@master/style';
 
 export class GridTemplateAreasStyle extends Style {
-    static override key = GRID + DASH + TEMPLATE + DASH + 'areas';
+    static override key = GRID + DASH + TEMPLATE + DASH + AREA + 's';
 }
