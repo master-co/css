d9ea0665603c237301ecb8f2ddc3feeaedaaa6e8
aron-tw <aron886tw@gmail.com>
2022-01-29 01:42:41 +0800

    feat: transform support px to rem

--- src/transform.ts
+++ src/transform.ts
@@ -1,4 +1,4 @@
-import { DEG, PX, ROTATE, SKEW, TRANSFORM, TRANSLATE } from './constants/css-property-keyword';
+import { DEG, REM, ROTATE, SKEW, TRANSFORM, TRANSLATE } from './constants/css-property-keyword';
 import { Style } from '@master/style';
 
 export class TransformStyle extends Style {
@@ -7,13 +7,13 @@ export class TransformStyle extends Style {
     static override unit = '';
     override get parseValue() {
         return this.value.replace(
-            /(translate|scale|skew|rotate|perspective|matrix)(3d|X|Y|Z)?\((.*?)\)(;|$)/gm,
+            /(translate|scale|skew|rotate|perspective|matrix)(3d|X|Y|Z)?\((.*?)\)/gm,
             (origin, method, type, valueStr: string) => {
                 let unit: string;
                 let last: boolean;
                 switch (method) {
                     case TRANSLATE:
-                        unit = PX;
+                        unit = REM;
                         break;
                     case SKEW:
                         unit = DEG;
@@ -21,7 +21,7 @@ export class TransformStyle extends Style {
                     case ROTATE:
                         if (type === '3d') {
                             last = true;
-                        }    
+                        }
                         unit = DEG;
                         break;
                     default:
@@ -32,9 +32,17 @@ export class TransformStyle extends Style {
                 return origin.replace(
                     valueStr,
                     values
-                        .map((eachValue, i) => (!last || values.length - 1 === i)
-                            ? eachValue + (Number.isNaN(+eachValue) ? '' : unit)
-                            : eachValue)
+                        .map((eachValue, i) => {
+                            if (!last || values.length - 1 === i) {
+                                const isNaN = Number.isNaN(+eachValue);
+                                return isNaN
+                                    ? eachValue
+                                    : ((eachValue as any) / (unit === REM ? 16 : 1)) // 將無單位的 / 16 轉為 rem
+                                    + unit;
+                            } else {
+                                return eachValue;
+                            }
+                        })
                         .join(','));
             })
     }
