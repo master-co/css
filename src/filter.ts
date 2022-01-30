f473bd55cce26757cde6806a5c3d4d1a14ff8c38
BenSeage <bg012300@gmail.com>
2022-01-30 13:20:22 +0800

    fix: filter parseValue

--- src/filter.ts
+++ src/filter.ts
@@ -1,6 +1,23 @@
-import { FILTER } from './constants/css-property-keyword';
+import { DASH, DEG, FILTER, REM, ROTATE, SHADOW } from './constants/css-property-keyword';
 import { Style } from '@master/style';
+import { parseValueUnit } from './utils/parse-value-unit';
 
 export class FilterStyle extends Style {
+    static override matches = /^blur\(.*?\)/
     static override key = FILTER;
+    override get parseValue() {
+        return parseValueUnit(
+            this.value,
+            method => {
+                switch (method) {
+                    case 'blur':
+                    case 'drop' + DASH + SHADOW:
+                        return REM;
+                    case 'hue' + DASH + ROTATE:
+                        return DEG;
+                }
+
+                return '';
+            });
+    }
 }
