6faa8acbf72d35ff4f07af9846a0d748c79bce98
Miles <swrev5741@gmail.com>
2022-01-31 16:24:06 +0800

    fix: text-decoration colorful

--- src/text-decoration.ts
+++ src/text-decoration.ts
@@ -4,4 +4,5 @@ import { Style } from '@master/style';
 export class TextDecorationStyle extends Style {
     static override matches =  /^t(ext)?:(underline|line-through|overline)/;
     static override key = TEXT + DASH + DECORATION;
+    static override colorful = true;
 }
