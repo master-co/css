907fc51ccc18b86edfd459a9ab36948d0510ef63
SerKo <serko.dev@gmail.com>
2022-04-20 03:45:42 +0800

    fix: remove text-stroke css property

--- src/text-stroke.ts
+++ src/text-stroke.ts
@@ -4,7 +4,6 @@ export class TextStrokeStyle extends Style {
     static override matches = /^text-stroke:./;
     override get props(): { [key: string]: any } {
         return {
-            'text-stroke': this,
             '-webkit-text-stroke': this
         };
     }
