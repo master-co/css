c4b9b0974c3f9bf9a711686808c1b371aaa6a26c
aron-tw <aron886tw@gmail.com>
2022-04-20 21:07:11 +0800

    feat: -webkit-line-clamp

---
+++ src/line-clamp.ts
@@ -0,0 +1,12 @@
+import { CLAMP, dash, LINE } from './constants/css-property-keyword';
+import { Style } from '@master/style';
+
+export class LineClamp extends Style {
+    static override key = dash(LINE, CLAMP);
+    static override unit = '';
+    override get props(): { [key: string]: any } {
+        return {
+            '-webkit-line-clamp': this
+        }
+    }
+}
