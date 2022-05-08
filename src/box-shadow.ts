4d680d138c1461a6aa2e24f277c9507b674c5d57
aron-tw <aron886tw@gmail.com>
2022-05-09 03:32:16 +0800

    feat: added acronym `s` for BoxShadow

--- src/box-shadow.ts
+++ src/box-shadow.ts
@@ -2,7 +2,7 @@ import { Style } from '@master/style';
 import { BOX, dash, SHADOW } from './constants/css-property-keyword';
 
 export class BoxShadow extends Style {
-    static override matches = /^shadow:./;
+    static override matches = /^s(?:hadow)?:./;
     static override key = dash(BOX, SHADOW);
     static override colorful = true;
 }
