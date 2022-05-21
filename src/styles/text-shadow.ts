5be74296a7bc3556b47284a8fb7741c447e7e2fb
aron-tw <aron886tw@gmail.com>
2022-05-21 14:59:22 +0800

    fix: text-shadow color tokens

--- src/styles/text-shadow.ts
+++ src/styles/text-shadow.ts
@@ -3,4 +3,5 @@ import { dash, SHADOW, TEXT } from '../constants/css-property-keyword';
 
 export class TextShadow extends Style {
     static override key = dash(TEXT, SHADOW);
+    static override colorful = true;
 }
