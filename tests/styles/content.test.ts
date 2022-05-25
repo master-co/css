5b4b338c6fdc5bb9b6a6bd5c829e64fc717fb9fa
BenSeage <bg012300@gmail.com>
2022-05-25 13:27:51 +0800

    test: content

--- tests/styles/content.test.ts
+++ tests/styles/content.test.ts
@@ -3,5 +3,5 @@ import { testCSS, testProp } from "../utils/test-css"
 test("content", () => {
     testCSS("content:'foo'", ".content\\:\\'foo\\'{content:'foo'}")
     testProp("content:'foo'")
-    testProp("content:'fo\'o'")
+    testProp("content:'fo\\\'o'")
 })
