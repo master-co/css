5b3908c7f464cacbe21aeb4f26d62a374cc50e18
aron-tw <aron886tw@gmail.com>
2022-05-28 00:24:35 +0800

    chore: test

--- tests/selectors.test.ts
+++ tests/selectors.test.ts
@@ -2,6 +2,9 @@ import { testCSS } from "./utils/test-css"
 
 test("breakpoints", () => {
     testCSS("hide@xs", "@media (min-width:600px){.hide\\@xs{display:none}}")
+})
+
+test("shorthands", () => {
     testCSS("hide:first", ".hide\\:first:first-child{display:none}")
     testCSS("hide:last", ".hide\\:last:last-child{display:none}")
     testCSS("hide:even", ".hide\\:even:nth-child(2n){display:none}")
