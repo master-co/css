3fcf75da04fedadf7aa387fd8c7314718507d67d
aron-tw <aron886tw@gmail.com>
2022-06-16 08:32:12 +0800

    test: transition

--- src/styles/transition.test.ts
+++ src/styles/transition.test.ts
@@ -1,5 +1,5 @@
 import { testCSS } from "../utils/test-css"
 
 test("transition", () => {
-    testCSS("~transform|.1s|ease-out,width|.1s|ease-out", ".\\\~transform\\\|\\\.1s\\\|ease-out\\\,width\\\|\\\.1s\\\|ease-out{transition:transform 0.1s ease-out,width 0.1s ease-out}")
+    testCSS("~transform|.1s|ease-out,width|.1s|ease-out", ".\\\~transform\\\|\\\.1s\\\|ease-out\\\,width\\\|\\\.1s\\\|ease-out{transition:transform 0.1s ease-out, width 0.1s ease-out}")
 })
