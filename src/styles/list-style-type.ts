897863669c7efc8b8bc60d2eeedf6394517c7a63
aron-tw <aron886tw@gmail.com>
2022-06-22 15:15:45 +0800

    fix: `list-style`

--- src/styles/list-style-type.ts
+++ src/styles/list-style-type.ts
@@ -2,6 +2,6 @@ import { dash, LIST, STYLE, TYPE } from '../constants/css-property-keyword';
 import { Style } from '../style';
 
 export class ListStyleType extends Style {
-    static override matches = /^list-style:(none|disc|decimal)(?!\|)/;
+    static override matches = /^list-style:(disc|decimal)(?!\|)/;
     static override key = dash(LIST, STYLE, TYPE);
 }
