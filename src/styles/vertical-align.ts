784a15b32d76b4d774f8cfed2211f7eba933143e
aron-tw <aron886tw@gmail.com>
2022-06-22 15:16:32 +0800

    feat: add `vertical` shorthand for `vertical-align`

--- src/styles/vertical-align.ts
+++ src/styles/vertical-align.ts
@@ -2,6 +2,6 @@ import { ALIGN, dash, VERTICAL } from '../constants/css-property-keyword';
 import { Style } from '../style';
 
 export class VerticalAlign extends Style {
-    static override matches = /^v:./;
+    static override matches = /^(?:v|vertical):./;
     static override key = dash(VERTICAL, ALIGN);
 }
