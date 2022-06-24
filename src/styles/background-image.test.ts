66c9b238dcf06354c2d5c3d40ef9c9744e6d0140
BenSeage <bg012300@gmail.com>
2022-06-24 21:37:52 +0800

    fix: parse color

--- src/styles/background-image.test.ts
+++ src/styles/background-image.test.ts
@@ -1,5 +1,5 @@
 import { testProp } from "../utils/test-css"
 
 test("background-image", () => {
-    testProp("bg:linear-gradient(0deg,var(--gray-14)|0%,var(--gray-16)|100%)", "background-image:linear-gradient(0deg,#1b1b1b 0%,#1e1e1e 100%)")
+    testProp("bg:linear-gradient(0deg,gray-14|0%,gray-16|100%)", "background-image:linear-gradient(0deg,#1b1b1b 0%,#1e1e1e 100%)")
 })
