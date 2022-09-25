619f62a6c64112f3585216621b6b7f5178369928
1Aron <aron886tw@gmail.com>
2022-09-25 21:57:26 +0800

    feat: add `text(?:-fill)?:(?:current|transparent)` for `-webkit-text-fill-color:transparent`

--- src/styles/text.test.ts
+++ src/styles/text.test.ts
@@ -4,5 +4,7 @@ test("text", () => {
     testProp("text:20", "font-size:1.25rem;line-height:1.625rem")
     testProp("text:current", "-webkit-text-fill-color:currentColor")
     testProp("text-fill:current", "-webkit-text-fill-color:currentColor")
+    testProp("text:transparent", "-webkit-text-fill-color:transparent")
+    testProp("text-fill:transparent", "-webkit-text-fill-color:transparent")
     testProp("text:#fff", "-webkit-text-fill-color:#fff")
 })
