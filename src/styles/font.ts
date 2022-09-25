918223a405391fca8d8f3566bd803d93ac35bc4b
1Aron <aron886tw@gmail.com>
2022-09-25 23:20:40 +0800

    fix: remove useless `Font.colorful`

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -5,6 +5,5 @@ export class Font extends Style {
     static override matches = /^f:./;
     static override key = FONT;
     static override unit = '';
-    static override colorful = true;
     override order = -1;
 }
