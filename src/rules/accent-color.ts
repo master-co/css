476830828a8ce9af9fa7e69db0175ba749ff3773
BenSeage <bg012300@gmail.com>
2022-10-08 02:25:44 +0800

    fix: `AccentColor` key

--- src/rules/accent-color.ts
+++ src/rules/accent-color.ts
@@ -1,7 +1,7 @@
 import { MasterCSSRule } from '../rule';
 
 export class AccentColor extends MasterCSSRule {
-    static override key = 'accentColor';
+    static override key = 'accent-color';
     static override colorStarts = 'accent:';
     static override colorful = true;
 }
