5140b2238ab16ecbdf992409eab6a05ef54a99dc
1Aron <aron886tw@gmail.com>
2022-10-11 12:33:31 +0800

    fix: variable matches

--- src/rules/variable.ts
+++ src/rules/variable.ts
@@ -2,7 +2,7 @@ import { MasterCSSRule } from '../rule';
 
 export class Variable extends MasterCSSRule {
     static id = 'variable';
-    static override matches = /^\$.+:./;
+    static override matches = /^\$[^ {}A-Z]+:[^ ]/;
     static override unit = ''; // don't use 'rem' as default, because css variable is common API
     override get(declaration): { [key: string]: any } {
         return {
