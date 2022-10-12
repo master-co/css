3a9e82426e157ff72d045b5d01087195fb2fbd9b
BenSeage <bg012300@gmail.com>
2022-10-12 09:50:50 +0800

    fix: scrollbar selectors

--- src/config/selectors.js
+++ src/config/selectors.js
@@ -1,5 +1,10 @@
 module.exports = {
     '::scrollbar': '::-webkit-scrollbar',
+    '::scrollbar-button': '::-webkit-scrollbar-button',
+    '::scrollbar-thumb': '::-webkit-scrollbar-thumb',
+    '::scrollbar-track': '::-webkit-scrollbar-track',
+    '::scrollbar-track-piece': '::-webkit-scrollbar-track-piece',
+    '::scrollbar-corner': '::-webkit-scrollbar-corner',
     '::slider-thumb': ['::-webkit-slider-thumb', '::-moz-range-thumb'],
     '::slider-runnable-track': ['::-webkit-slider-runnable-track', '::-moz-range-track'],
     '::meter': '::-webkit-meter',
