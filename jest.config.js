e72eb0cb59a05a6788199d3d286949beec0edac7
1Aron <aron886tw@gmail.com>
2022-10-19 04:57:17 +0800

    env: build

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
     transform: {
-        "^.+\\.(t|j)sx?$": ["@swc/jest"],
+        "^.+\\.(t|j)sx?$": 'esbuild-jest',
     },
 };
