df82ceadf17da01afc4d321a0f6ca2e743c6d4e3
1Aron <aron886tw@gmail.com>
2022-10-19 17:11:07 +0800

    env: @swc/jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "node",
+    preset: 'ts-jest',
+    testEnvironment: 'node',
     transform: {
-        "^.+\\.(t|j)sx?$": 'esbuild-jest',
+        '^.+\\.(t|j)sx?$': '@swc/jest',
     },
-};
+}
