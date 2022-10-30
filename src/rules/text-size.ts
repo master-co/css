327013bf031b2e4f2cf48e1b5c5cf5aa4148342f
1Aron <aron886tw@gmail.com>
2022-10-31 01:20:05 +0800

    fix: `text-size` diff

--- src/rules/text-size.ts
+++ src/rules/text-size.ts
@@ -1,16 +1,19 @@
 import MasterCSSRule from '../rule'
 
+const diff = .5
+
 export default class extends MasterCSSRule {
     static override id = 'TextSize'
     static override matches = /^t(ext)?:([0-9]|(max|min|calc|clamp)\(.*\))((?!\|).)*$/
     override get(declaration): { [key: string]: any } {
+        const { unit, value } = declaration
         return {
             'font-size': declaration,
             'line-height': {
                 ...declaration,
-                value: declaration.unit === 'rem'
-                    ? declaration.value + .375 + declaration.unit
-                    : 'calc(' + declaration.value + declaration.unit + ' + .375rem)',
+                value: unit === 'rem'
+                    ? value + diff + unit
+                    : `calc(${value}${unit} + ${diff}rem)`,
                 unit: ''
             }
         }
