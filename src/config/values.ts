ddbe3fde41eea368c7ec04a37cd74ef59e70fbb9
1Aron <aron886tw@gmail.com>
2022-11-11 06:49:49 +0800

    perf: minify `values.FontFamily`

--- src/config/values.ts
+++ src/config/values.ts
@@ -40,9 +40,9 @@ const values = {
         'col-reverse': 'column-reverse'
     },
     FontFamily: {
-        mono: 'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, Liberation Mono, Courier New, monospace',
-        sans: 'ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji',
-        serif: 'ui-serif, Georgia, Cambria, Times New Roman, Times, serif'
+        mono: 'ui-monospace,SFMono-Regular,Menlo,Monaco,Consolas,Liberation Mono,Courier New,monospace',
+        sans: 'ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji',
+        serif: 'ui-serif,Georgia,Cambria,Times New Roman,Times,serif'
     },
     FontWeight: {
         thin: 100,
