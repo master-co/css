781e235e62e8e4db2cebcc334f0e06c8eae67d13
1Aron <aron886tw@gmail.com>
2022-11-12 23:42:51 +0800

    chore: remove changesets, add jest

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('jest').Config} */
+export default {
+    preset: 'jest-puppeteer',
+    testEnvironment: 'jest-environment-jsdom',
+    transform: {
+        '^.+\\.(t|j)sx?$': '@swc/jest'
+    },
+    'globals': {
+        'ts-jest': {
+            'tsConfig': {
+                'importHelpers': true
+            }
+        }
+    }
+}
