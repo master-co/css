4e96714ca3d16335f153cfa3ede73b486c978be3
BenSeage <bg012300@gmail.com>
2022-11-22 11:32:52 +0800

    Fix(CSS): `foreground|fg` missed CSS variables

--- packages/css/src/rules/color.ts
+++ packages/css/src/rules/color.ts
@@ -2,7 +2,7 @@ import Rule from '../rule'
 
 export default class extends Rule {
     static override id = 'Color' as const
-    static override colorStarts = '(?:color|fg|foreground):'
+    static override matches = '^(?:color|fg|foreground):.'
     static override colorful = true
     static override unit = ''
 }
