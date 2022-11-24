59021f3bf860458b1771a9f64052d72fe9e1e40a
1Aron <aron886tw@gmail.com>
2022-11-24 13:06:43 +0800

    Example(CSS): Fix svelte-kit

--- examples/svelte-kit/vite.config.ts
+++ examples/svelte-kit/vite.config.ts
@@ -1,10 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 import MasterCSSVitePlugin from '@master/css.vite'
+import path from 'path'
 
 const config: UserConfig = {
     plugins: [
-        MasterCSSVitePlugin(),
+        MasterCSSVitePlugin({
+            output: {
+                name: 'master.css',
+                dir: path.join(process.cwd(), 'static')
+            }
+        }),
         sveltekit()
     ]
 };
