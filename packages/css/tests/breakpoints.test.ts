4f3f708e743fd6a49f2be1ec666dcdcec225b9f2
1Aron <aron@master.co>
2022-12-04 03:26:15 +0800

    Misc: Tests

--- packages/css/tests/breakpoints.test.ts
+++ packages/css/tests/breakpoints.test.ts
@@ -1,5 +1,5 @@
 import { testCSS } from '../src/utils/test-css'
-import MasterCSS from '..'
+import MasterCSS from '../src'
 import config from '../../../master.css.js'
 
 test('breakpoints', () => {
