4c1993407359b175b0b05c983855e32723748f45
1Aron <aron@master.co>
2022-12-04 23:44:36 +0800

    Test(CSS): Exclude invalid class names

---
+++ packages/css/tests/exceptions.test.ts
@@ -0,0 +1,8 @@
+import MasterCSS from '../src'
+
+test('exception handling', async () => {
+    const css = new MasterCSS()
+    expect(css.create('master:css').length).toBe(0)
+    expect(css.create('{/if}').length).toBe(0)
+    expect(css.create('fg:blue').length).toBe(1)
+})
