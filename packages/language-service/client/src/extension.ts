ece874acd01bc629ffa6a8c20ff22aedea651f2d
Miles <swrev5741@gmail.com>
2022-12-05 21:31:18 +0800

    Fix(Language Service): Language server path

--- packages/language-service/client/src/extension.ts
+++ packages/language-service/client/src/extension.ts
@@ -18,7 +18,7 @@ export function activate(context: ExtensionContext) {
 
     // The server is implemented in node
     const serverModule = context.asAbsolutePath(
-        path.join('server', 'out', 'server.js')
+        path.join('server', 'dist', 'server.cjs')
     )
     // The debug options for the server
     // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
