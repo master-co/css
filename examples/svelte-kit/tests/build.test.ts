583ab968e557e70b9522caef83c16f3f03e13706
1Aron <aron@master.co>
2022-12-15 22:19:34 +0800

    Example(Vite): Update to Vite v4

--- examples/svelte-kit/tests/build.test.ts
+++ examples/svelte-kit/tests/build.test.ts
@@ -5,7 +5,7 @@ execSync('rm -rf .svelte-kit', { stdio: 'inherit'  })
 execSync('npm run build', { stdio: 'inherit'  })
 
 test('check that the output contains the expected css rules', () => {
-    expectFileIncludes('../.svelte-kit/output/client/_app/immutable/assets/src/routes/master-*.css', [
-        'bg\\:gray-90'
+    expectFileIncludes('../.svelte-kit/output/client/_app/immutable/assets/_layout-*.css', [
+        'bg\\:green-90'
     ])
 })
--- examples/vite/src/main.ts
+++ examples/vite/src/main.ts
@@ -1,6 +1,25 @@
-import { createApp } from 'vue'
 import './style.css'
 import 'master.css'
-import App from './App.vue'
+import { setupCounter } from './counter'
 
-createApp(App).mount('#app')
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <div class="flex center-content">
+        <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://css.master.co" target="_blank">
+        <img src="/master.svg" class="logo w:172 h:172" alt="Master logo" />
+        </a>
+    </div>
+    <h1>Vite v4 + Master CSS v2</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and Master CSS logos to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
