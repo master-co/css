60dae0b772ad38afbb2bc3710afb856aa182e66a
1Aron <aron@master.co>
2022-12-15 22:19:34 +0800

    Example(Vite): Update to Vite v4

--- examples/vite/vite.config.ts
+++ examples/vite/vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import { MasterCSSVitePlugin } from '@master/css.vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        MasterCSSVitePlugin({ debug: true }),
-        vue()
+        MasterCSSVitePlugin({ debug: true })
     ]
 })
