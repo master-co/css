b6544a579d918bafaecebb77b9be21ebf0b0f1f9
1Aron <aron@master.co>
2022-12-15 23:50:34 +0800

    Example(Next.js 13): Update to Next.js 13

--- examples/next.js/pages/api/hello.ts
+++ examples/next.js/pages/api/hello.ts
@@ -2,12 +2,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+    name: string
 }
 
 export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+    res.status(200).json({ name: 'John Doe' })
 }
