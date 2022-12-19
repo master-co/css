2806e6d7087c8524afffb457d557a8c874705bc2
BenSeage <bg012300@gmail.com>
2022-12-19 19:48:50 +0800

    Update astro example

--- examples/astro/astro.config.mjs
+++ examples/astro/astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import { MasterCSSVitePlugin } from '@master/css.vite'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        plugins: [MasterCSSVitePlugin()]
+    }
+})
