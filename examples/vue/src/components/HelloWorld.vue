9338cd30a9a40c1af386a4f11c64d08c2b191c7d
1Aron <aron@master.co>
2022-12-19 21:37:51 +0800

    Example(Vue): Add Vue example for AOT and JIT

---
+++ examples/vue/src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+
+    <div class="card">
+        <button type="button" @click="count++" class="bg:purple bg:purple-55:hover h:48 px:25">
+            count is {{ count }}
+        </button>
+        <p>
+            Edit
+            <code>components/HelloWorld.vue</code> to test HMR
+        </p>
+    </div>
+
+    <p>
+        Check out
+        <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+        starter
+    </p>
+    <p>
+        Install
+        <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+        in your IDE for a better DX
+    </p>
+    <p class="read-the-docs">Click on the Vue and Master CSS logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+    color: #888;
+}
+</style>
---
+++ examples/vue/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { MasterCSSVitePlugin } from '@master/css.vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        vue(),
+        MasterCSSVitePlugin({ debug: true })
+    ],
+})
