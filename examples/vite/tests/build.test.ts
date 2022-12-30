c0b6bec1993ae7638608c1243788ed001d4141f1
BenSeage <bg012300@gmail.com>
2022-12-30 11:21:36 +0800

    Test(Vite): Fix path error

--- examples/vite/tests/build.test.ts
+++ examples/vite/tests/build.test.ts
@@ -1,6 +1,7 @@
 import { spawnSync } from 'child_process'
 import MasterCSSCompiler from '@master/css-compiler'
 import { expectFileIncludes } from '../../../utils/expect-file-includes'
+import upath from 'upath'
 
 it('check if dist contains virtual:master.css result ( usually bundled with styles.css )', async () => {
     spawnSync('npm.cmd', ['run', 'build'])
@@ -9,8 +10,7 @@ it('check if dist contains virtual:master.css result ( usually bundled with styl
     await compiler.init()
 
     expectFileIncludes(
-        'dist/assets/index-*.css', 
-        Object.keys(compiler.css.ruleOfClass).map(CSS.escape),
-        process.cwd()
+        upath.join('..', 'dist', 'assets', 'index-*.css'),
+        Object.keys(compiler.css.ruleOfClass).map(CSS.escape)
     )
 })
--- utils/expect-file-includes.ts
+++ utils/expect-file-includes.ts
@@ -2,9 +2,11 @@ import fs from 'fs'
 import upath from 'upath'
 import fg from 'fast-glob'
 
-export function expectFileIncludes(filePath: string, includes: string[], parentModuleDir = '') {
+const parentModuleDir = upath.dirname(require.main.filename)
+
+export function expectFileIncludes(filePath: string, includes: string[]) {
     const content = fs.readFileSync(
-        fg.sync(upath.join(parentModuleDir || upath.dirname(require.main.filename), filePath))[0]
+        fg.sync(upath.join(parentModuleDir, filePath))[0]
     ).toString()
     includes.map((include) => {
         expect(content).toContain(include)
