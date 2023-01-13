9f794a5d9a7dece1af41bd74a46f990688438c7f
1Aron <aron@master.co>
2023-01-13 15:49:14 +0800

    Example(Lit): Add the lit.dev example

---
+++ examples/lit/vite.config.ts
@@ -0,0 +1,10 @@
+import { MasterCSSVitePlugin } from '@master/css.vite'
+
+/** @type {import('vite').UserConfig} */
+const config = {
+    plugins: [
+        MasterCSSVitePlugin()
+    ]
+}
+
+export default config
