fa97d0ca49f06369360919e7023437eb6f872a3b
1Aron <aron@master.co>
2023-03-02 20:36:23 +0800

    New(React): `CSSLazyProvider` for lazy loading JIT engine

---
+++ packages/react/src/contexts/lazy-provider.tsx
@@ -0,0 +1,27 @@
+import type { Config, MasterCSS } from '@master/css'
+import { ReactElement, useEffect, useState } from 'react'
+import { CSSContext } from './css'
+
+export const CSSLazyProvider = ({
+    children,
+    config,
+    root = typeof document !== 'undefined' ? document : null
+}: {
+    children: ReactElement,
+    config?: Config | Promise<any>,
+    root?: Document | ShadowRoot | null
+}) => {
+    const [css, setCSS] = useState<MasterCSS>()
+    useEffect(() => {
+        if (!css) {
+            (async () => {
+                const { MasterCSS } = await import('@master/css')
+                const existingCSS = MasterCSS.instances.find((eachCSS) => eachCSS.root === root)
+                const configModule = await config
+                const resolvedConfig = configModule.default || configModule.config || configModule
+                setCSS(existingCSS || new MasterCSS({ ...resolvedConfig }))
+            })()
+        }
+    }, [config, css, root])
+    return <CSSContext.Provider value={css}>{children}</CSSContext.Provider>
+}
