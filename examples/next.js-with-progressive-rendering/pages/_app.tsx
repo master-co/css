940373d2fbf78524bbdc0078b6baa697d28b3e11
1Aron <aron@master.co>
2023-03-11 07:52:21 +0800

    Update examples

--- examples/next.js-with-progressive-rendering/pages/_app.tsx
+++ examples/next.js-with-progressive-rendering/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '~/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { CSSLazyProvider, ThemeProvider } from '@master/css.react'
+import { CSSLazyProvider } from '@master/css.react'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
