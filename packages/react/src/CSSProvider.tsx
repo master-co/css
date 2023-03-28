ff19f2431c59b701b4604ff7c37a78af08719295
1Aron <aron@master.co>
2023-03-28 18:19:42 +0800

    Fix(React): Infinite renders

--- packages/react/src/CSSProvider.tsx
+++ packages/react/src/CSSProvider.tsx
@@ -1,7 +1,9 @@
 import { Config, MasterCSS } from '@master/css'
-import { ReactElement, useEffect, useLayoutEffect, useState, useMemo, Context, createContext, useContext } from 'react'
+import { ReactElement, useEffect, useLayoutEffect, Context, createContext, useContext, EffectCallback, DependencyList, useState } from 'react'
 
 const { instances } = MasterCSS
+const useIsomorphicEffect: (effect: EffectCallback, deps?: DependencyList) => void =
+    typeof window !== 'undefined' ? useLayoutEffect : useEffect
 export const CSSContext: Context<MasterCSS> = createContext<MasterCSS>(null)
 
 export function useCSS() {
@@ -17,20 +19,16 @@ export const CSSProvider = ({
     config?: Config,
     root?: Document | ShadowRoot | null
 }) => {
-    const [css, setCSS] = useState<MasterCSS>()
     const existingCSS = instances.find((eachCSS) => eachCSS.root === root)
-    if (existingCSS) {
-        setCSS(existingCSS)
-    } else {
-        setCSS(new MasterCSS({ ...config, observe: false }))
-    }
-
-    (typeof window !== 'undefined' ? useLayoutEffect : useEffect)(() => {
-        if (!css.style) css.observe(root)
-        return () => {
-            css.destroy()
+    const [css] = useState<MasterCSS>(existingCSS || new MasterCSS({ ...config, observe: false }))
+    useIsomorphicEffect(() => {
+        if (!css.observing) {
+            css.observe(root)
+        } else {
+            return () => {
+                css.destroy()
+            }
         }
     }, [css, root])
-
     return <CSSContext.Provider value={css}>{children}</CSSContext.Provider>
 }
