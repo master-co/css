7752d56a01fc089d6a59bb2807a22fe963a363b3
1aron <i@aron.tw>
2023-04-05 00:31:14 +0800

    Update tests

--- examples/vite/tests/dev.test.ts
+++ examples/vite/tests/dev.test.ts
@@ -1,27 +1,31 @@
 import { ChildProcess } from 'child_process'
 import { runViteDevProcess } from '../../vite/tests/run-vite-dev-process'
+import puppeteer, { type Browser, type Page } from 'puppeteer'
 
 let devProcess: ChildProcess
 let error: Error
-
-page.on('console', (consoleMessage) => {
-    if (consoleMessage.type() === 'error') {
-        error = new Error(consoleMessage.text())
-    }
-})
-page.on('pageerror', (e) => error = e)
-page.on('error', (e) => error = e)
+let browser: Browser
+let page: Page
 
 beforeAll(async () => {
-    devProcess = await runViteDevProcess()
+    browser = await puppeteer.launch()
+    page = await browser.newPage()
+    page.on('console', (consoleMessage) => {
+        if (consoleMessage.type() === 'error') {
+            error = new Error(consoleMessage.text())
+        }
+    })
+    page.on('pageerror', (e) => error = e)
+    page.on('error', (e) => error = e)
+    devProcess = await runViteDevProcess(page)
 })
 
 it('run dev without errors', () => {
     expect(() => { if (error) throw error }).not.toThrowError()
 })
 
-afterAll((done) => {
+afterAll(async () => {
     devProcess.stdout?.destroy()
     devProcess.kill()
-    done()
+    await browser.close()
 })
