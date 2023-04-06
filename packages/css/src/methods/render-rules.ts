0104b613f63346227165f3aee7063571fb276871
1aron <i@aron.tw>
2023-04-06 15:34:45 +0800

    Add(CSS): `renderRules()` method

---
+++ packages/css/src/methods/render-rules.ts
@@ -0,0 +1,15 @@
+import { MasterCSS } from '../css'
+import type { Config } from '../config'
+import type { Rule } from '../rule'
+
+export function renderRules(classes: string[], config?: Config): Rule[] {
+    if (!classes?.length) return
+    const css = new MasterCSS({ ...config, observe: false })
+    for (const eachClassName of classes) {
+        if (!(eachClassName in css.countBy)) {
+            if (css.insert(eachClassName))
+                css.countBy[eachClassName] = 1
+        }
+    }
+    return css.rules
+}
