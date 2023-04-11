a878e7963107e47a6de2deb2ccf8a0b4775b0d64
1aron <i@aron.tw>
2023-04-11 18:24:35 +0800

    Tests(Compiler): Build and output `./master.css`

---
+++ packages/compiler/tests/build/test.ts
@@ -0,0 +1,16 @@
+import { execSync } from 'child_process'
+import { expectFileIncludes } from '../../../../utils/expect-file-includes'
+import '../../../css/src/polyfills/css-escape'
+
+it('build master.css', () => {
+    execSync('node ../../../css/dist/cjs/bin init -c -o', { cwd: __dirname })
+    execSync('node ../../../css/dist/cjs/bin', { cwd: __dirname })
+    expectFileIncludes('master.css', [
+        CSS.escape('fg:primary'),
+        CSS.escape('m:50'),
+        CSS.escape('text:center'),
+        CSS.escape('font:sans'),
+        CSS.escape('font:heavy'),
+        CSS.escape('font:48')
+    ])
+})
