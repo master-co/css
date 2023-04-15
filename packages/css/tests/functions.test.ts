3f6ad0210a22b4cae2626fa247cb35baee9e497f
BenSeage <bg012300@gmail.com>
2023-04-15 19:11:25 +0800

    Fix(CSS): Normalize that `()` in `calc` is missing spaces #206

--- packages/css/tests/functions.test.ts
+++ packages/css/tests/functions.test.ts
@@ -15,4 +15,9 @@ test('functions', () => {
     testCSS('box-shadow:0|2|3|rgba(0,0,0,.1)', '.box-shadow\\:0\\|2\\|3\\|rgba\\(0\\,0\\,0\\,\\.1\\){box-shadow:0rem 0.125rem 0.1875rem rgba(0,0,0,.1)}')
 
     testCSS('grid-template-cols:repeat(2,auto|.6|calc(3-max(2,1)))', '.grid-template-cols\\:repeat\\(2\\,auto\\|\\.6\\|calc\\(3-max\\(2\\,1\\)\\)\\){grid-template-columns:repeat(2,auto .6 calc(0.1875rem - max(0.125rem, 0.0625rem)))}')
+    
+    testCSS('mt:calc($(g-y)*-.1)', '.mt\\:calc\\(\\$\\(g-y\\)\\*-\\.1\\){margin-top:calc(var(--g-y) * -0.00625rem)}')
+    testCSS('mt:calc($(g-y)*(-.1))', '.mt\\:calc\\(\\$\\(g-y\\)\\*\\(-\\.1\\)\\){margin-top:calc(var(--g-y) * (-0.00625rem))}')
+    testCSS('mt:calc($(g-y)--.1)', '.mt\\:calc\\(\\$\\(g-y\\)--\\.1\\){margin-top:calc(var(--g-y) - -0.00625rem)}')
+    testCSS('mr:calc($(g-x)/(-2px))', '.mr\\:calc\\(\\$\\(g-x\\)\\/\\(-2px\\)\\){margin-right:calc(var(--g-x) / (-2px))}')
 })
