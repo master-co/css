10f9f5ce07b12c80846119f593a57f0e8cd642b0
BenSeage <bg012300@gmail.com>
2023-05-02 19:00:05 +0800

    Update examples

--- examples/next.js-with-just-in-time/app/layout.tsx
+++ examples/next.js-with-just-in-time/app/layout.tsx
@@ -13,9 +13,11 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <CSSProvider>
-                {children}
-            </CSSProvider>
+            <body>
+                <CSSProvider>
+                    {children}
+                </CSSProvider>
+            </body>
         </html>
     )
 }
