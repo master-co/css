1387b82aed1761d5497fcdc30d7c6b4f396e74aa
BenSeage <bg012300@gmail.com>
2023-05-06 14:43:59 +0800

    Example(Next.js): Upgrade to Next.js v13.4 App Router

--- examples/next.js-with-ahead-of-time/next.config.js
+++ examples/next.js-with-ahead-of-time/next.config.js
@@ -2,14 +2,15 @@ const { MasterCSSWebpackPlugin } = require('@master/css.webpack')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+    experimental: {
+        appDir: true,
+    },
     webpack: (config) => {
         config.plugins.push(
             new MasterCSSWebpackPlugin()
         )
         return config
     }
-
 }
 
 module.exports = nextConfig
