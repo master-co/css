e95f9e5626aa2af3805e05533a3681503fa3fb2b
BenSeage <bg012300@gmail.com>
2023-05-09 09:40:28 +0800

    Fix(CSS): `calc` transforms the value incorrectly

--- packages/css/src/config/functions.ts
+++ packages/css/src/config/functions.ts
@@ -12,90 +12,81 @@ export const functions: Record<string, FunctionConfig> = {
             const values = this.values
             const globalValues = this.css.globalValues
 
-            let newValue = '', type: 1 | 2, current = '', withUnit = false, functionName = '', isFirstChar = true
-            const clear = (char: string, prefix = '', suffix = '') => {
-                if (type === 2 && !withUnit) {
-                    const result = this.analyzeUnitValue(current, functions.calc.unit)
-                    if (result) {
-                        current = result.value + result.unit
-                    }
-                }
-
-                if (char !== '(') {
-                    if (values && current in values) {
-                        current = values[current].toString()
-                    } else if (globalValues && current in globalValues) {
-                        current = globalValues[current].toString()
-                    }
-                } else {
-                    functionName = current
-                }
-
-                if (char === ')') {
-                    functionName = ''
-                }
-
-                newValue += current + prefix + char + suffix
-
-                type = null
-                withUnit = false
-                current = ''
-            }
-
-            for (let i = 0; i < value.length; i++) {
-                const char = value[i]
-                if (char === '(' || char === ')') {
-                    clear(char)
-                    isFirstChar = true
-                    continue
-                } else if (char === ',') {
-                    clear(char, '', ' ')
-                } else if (char === '-' && type !== 1) {
-                    const previousValue = value[i - 1]
-                    if (previousValue === '(') {
-                        newValue += char
-                    } else if (previousValue === ')') {
-                        clear(char, ' ', ' ')
-                    } else if (isNaN(+value[i + 1])) {
-                        if (previousValue === '-') {
-                            clear(char)
-                            type = 1
-                        } else {
-                            clear(char, isFirstChar ? '' : ' ', isFirstChar ? '' : ' ')
+            // eslint-disable-next-line @typescript-eslint/no-this-alias
+            const instance = this
+            let i = 0, newValue = '', current = '';
+            (function anaylze(functionName: string, bypassHandlingUnitForcely: boolean) {
+                let bypassHandlingUnit = false
+                const clear = (char: string, prefix = '', suffix = '') => {
+                    if (char !== '(') {
+                        if (values && current in values) {
+                            current = values[current].toString()
+                        } else if (globalValues && current in globalValues) {
+                            current = globalValues[current].toString()
                         }
-                    } else {
-                        clear(char, current ? ' ' : '', current ? ' ' : '')
                     }
-                } else if (char === '+' || char === '*' || char === '/' || functionName !== 'var' && char === '-') {
-                    clear(char, ' ', ' ')
-                } else {
-                    switch (type) {
-                        // 字串
-                        case 1:
-                            break
-                        // 數字
-                        case 2:
-                            if (char !== '.' && !/[0-9]/.test(char)) {
-                                withUnit = true
-                            }
-                            break
-                        default:
-                            type = isNaN(+char)
-                                ? 1
-                                : 2
+    
+                    if (current && !bypassHandlingUnit && !bypassHandlingUnitForcely) {
+                        const result = instance.analyzeUnitValue(current, functions.calc.unit)
+                        if (result) {
+                            current = result.value + result.unit
+                        }
                     }
+    
+                    newValue += current + prefix + char + suffix
+    
+                    current = ''
+                    bypassHandlingUnit = false
+                }
 
-                    if (type) {
-                        current += char
+                for (;i < value.length; i++) {
+                    const char = value[i]
+                    if (char === '(') {
+                        const newFunctionName = current
+                        const originalBypassHandlingUnit = bypassHandlingUnit
+                        clear(char)
+                        i++
+                        anaylze(newFunctionName, originalBypassHandlingUnit || bypassHandlingUnitForcely)
+                    } else if (char === ')') {
+                        clear(char)
+                        break
+                    } else if (char === ',') {
+                        clear(char, '', ' ')
                     } else {
-                        newValue += char
+                        const previousChar = value[i - 1]
+                        switch (char) {
+                            case '+':
+                                if (!current && previousChar !== ')') {
+                                    current += char
+                                } else {
+                                    clear(char, ' ', ' ')
+                                }
+                                break
+                            case '-':
+                                if (functionName === 'var' || !current && previousChar !== ')') {
+                                    current += char
+                                } else {
+                                    clear(char, ' ', ' ')
+                                }
+                                break
+                            case '*':
+                                clear(char, ' ', ' ')
+                                break
+                            case '/':
+                                clear(char, ' ', ' ')
+                                bypassHandlingUnit = true
+                                break
+                            default:
+                                current += char
+                                break
+                        }
                     }
                 }
 
-                isFirstChar = false
-            }
-
-            clear('')
+                if (i >= value.length - 1) {
+                    clear('')
+                }
+            })('', false)
 
             return newValue
         }
--- packages/css/tests/values.test.ts
+++ packages/css/tests/values.test.ts
@@ -103,46 +103,28 @@ test('values', () => {
     )
     testCSS(
         'w:calc(-2+1x)',
-        '.w\\:calc\\(-2\\+1x\\){width:calc(-0.125rem + 60)}',
+        '.w\\:calc\\(-2\\+1x\\){width:calc(-0.125rem + 3.75rem)}',
         {
             values: {
-                '1x': '60'
+                '1x': 60
             }
         }
     )
     testCSS(
         'w:calc(-2-1x)',
-        '.w\\:calc\\(-2-1x\\){width:calc(-0.125rem - 60)}',
+        '.w\\:calc\\(-2-1x\\){width:calc(-0.125rem - 3.75rem)}',
         {
             values: {
-                '1x': '60'
+                '1x': 60
             }
         }
     )
     testCSS(
         'w:calc(-1x-2)',
-        '.w\\:calc\\(-1x-2\\){width:calc(-60 - 0.125rem)}',
+        '.w\\:calc\\(-1x-2\\){width:calc(-3.75rem - 0.125rem)}',
         {
             values: {
-                '1x': '60'
-            }
-        }
-    )
-    testCSS(
-        'w:calc(-x1-2)',
-        '.w\\:calc\\(-x1-2\\){width:calc(-60 - 0.125rem)}',
-        {
-            values: {
-                'x1': '60'
-            }
-        }
-    )
-    testCSS(
-        'w:calc(-2-x1)',
-        '.w\\:calc\\(-2-x1\\){width:calc(-0.125rem - 60)}',
-        {
-            values: {
-                'x1': '60'
+                '1x': 60
             }
         }
     )
