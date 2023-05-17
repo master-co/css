22f6b4d762db8259bebd79cc70fa1e20190f4812
1aron <i@aron.tw>
2023-05-17 15:08:08 +0800

    Rename `generateDocumentInitScript` to `getPreInitScript`

---
+++ packages/theme-service/src/methods/get-pre-init-script.ts
@@ -0,0 +1,28 @@
+import defaultOptions, { Options } from '../options'
+
+export function getPreInitScript(options?: Options) {
+    options = Object.assign(defaultOptions, options)
+    return `let e${options.default ? `='${options.default}'` : ''};const c=localStorage.getItem("${options.store}");c&&(e=c);let t=e;e==="system"&&(t=matchMedia("(prefers-color-scheme:dark)").matches?"dark":"light");const s=document.documentElement;s.classList.add(t);if(t==='dark'||t==='light'){s.style.colorScheme=t;};`
+}
+
+// 原始碼參考
+// export function getPreInitScript(options: Options = { store: 'theme' }) {
+//     return `
+//         let value = ${options.default};
+//         ${options.store ? `
+//             const storage = localStorage.getItem('${options.store}');
+//             if (storage) {
+//                 value = storage;
+//             }
+//         ` : ''}
+//         let current = value;
+//         if (value === 'system') {
+//             current = matchMedia('(prefers-color-scheme:dark)').matches ? 'dark' : 'light';
+//         };
+//         const host = document.documentElement;
+//         host.classList.add(current);
+//         if (current === 'dark' || current === 'light') {
+//             host.style.colorScheme = current;
+//         }
+//     `
+// }
