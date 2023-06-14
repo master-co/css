8c6dc604e8bbca22075367d9afb23c31af9f8b5c
1aron <i@aron.tw>
2023-06-14 19:51:52 +0800

    Add(CSS): `generateFromHTML()` function

---
+++ packages/css/src/functions/generate-from-html.ts
@@ -0,0 +1,18 @@
+import { Config } from '../config'
+import { MasterCSS } from '../core'
+import extractClassesFromHTML from './extract-classes-from-html'
+import generateFromClasses from './generate-from-classes'
+
+/**
+ * Generates the sorted CSS text from HTML
+ * @param html 
+ * @param config 
+ * @returns joined css text
+ */
+export default function generateFromHTML(html: string, config?: Config) {
+    if (!html) return
+    const css = new MasterCSS({ ...config, observe: false })
+    const classes = extractClassesFromHTML(html)
+    if (!classes.length) return
+    return generateFromClasses(classes, config)
+}
