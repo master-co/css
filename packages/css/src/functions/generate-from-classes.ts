49cf3465606e0914275412f4f774c17e3afd5b87
1aron <i@aron.tw>
2023-06-14 19:51:41 +0800

    Add(CSS): `generateFromClasses()` function

---
+++ packages/css/src/functions/generate-from-classes.ts
@@ -0,0 +1,17 @@
+import { Config } from '../config'
+import { MasterCSS } from '../core'
+
+/**
+ * Generates the sorted CSS text from classes
+ * @param classes
+ * @param config
+ * @returns joined css text
+ */
+export default function generateFromClasses(classes: string[], config?: Config) {
+    if (!classes.length) return
+    const css = new MasterCSS({ ...config, observe: false })
+    for (const eachClass of classes) {
+        css.insert(eachClass)
+    }
+    return css.text
+}
