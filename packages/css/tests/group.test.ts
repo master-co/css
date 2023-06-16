c63e003d55ce8018409aa886ff0c7920330c86c3
1aron <i@aron.tw>
2023-06-16 20:25:27 +0800

    Update CSS tests

--- packages/css/tests/group.test.ts
+++ packages/css/tests/group.test.ts
@@ -8,15 +8,20 @@ test('group', () => {
     testCSS('{line-height:calc(32-16);font-size:calc(32-16)}', '.\\{line-height\\:calc\\(32-16\\)\\;font-size\\:calc\\(32-16\\)\\}{line-height:calc(32 - 16);font-size:calc(2rem - 1rem)}')
     testCSS('{m:32;lh:1.5}', '.\\{m\\:32\\;lh\\:1\\.5\\}{margin:2rem;line-height:1.5}')
     testCSS(
-        '{content:\'\';abs;inset:0;bg:linear-gradient(90deg,G-10/.1|10%,G-20/.2|20%,G-30/.3|60%,white/.4)}::after', 
+        '{content:\'\';abs;inset:0;bg:linear-gradient(90deg,G-10/.1|10%,G-20/.2|20%,G-30/.3|60%,white/.4)}::after',
         '.\\{content\\:\\\'\\\'\\;abs\\;inset\\:0\\;bg\\:linear-gradient\\(90deg\\,G-10\\/\\.1\\|10\\%\\,G-20\\/\\.2\\|20\\%\\,G-30\\/\\.3\\|60\\%\\,white\\/\\.4\\)\\}\\:\\:after::after{content:\'\';position:absolute;inset:0rem;background-image:linear-gradient(90deg,#3333331A 10%,#66666633 20%,#9999994D 60%,#ffffff66)}',
         {
-            colors:{
-                G:{
-                    10:'#333333',
-                    20:'#666666',
-                    30:'#999999',
+            colors: {
+                G: {
+                    10: '#333333',
+                    20: '#666666',
+                    30: '#999999',
                 }
             }
         })
+})
+
+it('contains illegal syntax in group', () => {
+    testCSS('{form}', '')
+    testCSS('{form;block}', '.\\{form\\;block\\}{display:block}')
 })
