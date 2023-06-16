3dd94ff35a32dbf27391495ff6a00962353cc6d2
BenSeage <bg012300@gmail.com>
2023-06-16 20:36:39 +0800

    New: Class Variant #191

---
+++ packages/class-variant/src/index.tsx
@@ -0,0 +1,134 @@
+import line from '@techor/one-liner'
+
+type Param<T> = string 
+    | string[]
+    | Record<string, boolean>
+    | [string, { [key in keyof T]?: T[key] }]
+    | { [key in keyof T]?: Record<string, T[key]> }
+    | ((valueByProp: T) => any)
+type ReturnType<T> = { default?: Partial<T> } & ((valueByProp?: T) => string) 
+
+/**
+ * 1. 'inline-flex rounded'
+ * 2. ['inline-flex', 'rounded']
+ * 3. { 'bg:red': true, 'fg:30': false }
+ * 4. ['uppercase', { intent: 'primary', size: 'md' }]
+ * 5. { intent: { primary: 'bg:blue-50 fg:white', secondary: 'bg:white fg:gray-80' }, size: { sm: 'font:20 py:1 px:2', md: 'font:16 py:2 px:4' } }
+ * 6. ({ $intent, $size }) => $intent && $size && 'font:italic'
+ */
+function cv<T extends Record<string, string | number | boolean>>(...params: Array<Param<T>>): ReturnType<T>
+function cv<T extends Record<string, string | number | boolean>>(firstParam: TemplateStringsArray, ...params: Array<Param<T>>): ReturnType<T>
+function cv<T extends Record<string, string | number | boolean>>(firstParam: TemplateStringsArray | Param<T>, ...params: Array<Param<T>>): ReturnType<T> {
+    return function getClassNames(valueByProp: T = {} as any) {
+        const mergedValueByProp = Object.assign({}, getClassNames['default'], valueByProp)
+        const classesConditions: [string, Record<string, string | number | boolean>][] = []
+        const valuesByProp: Record<string, Record<string, string>> = {}
+        const handledParams = []
+        const handleParam = (param: Param<T>) => {
+            switch (typeof param) {
+                case 'object':
+                    if (typeof param === 'object') {
+                        if (Array.isArray(param)) {
+                            const newClassesByCondition = param[1]
+                            if (newClassesByCondition && typeof newClassesByCondition === 'object') {
+                                handledParams.push(param)
+        
+                                const keys = Object.keys(newClassesByCondition)
+            
+                                let duplicated = false
+                                for (const eachClassesByCondition of classesConditions) {
+                                    const entries = Object.entries(eachClassesByCondition[1])
+                                    if (
+                                        entries.length === keys.length
+                                        && entries.every(([key, value]) => keys.includes(key) && newClassesByCondition[key] === value)
+                                    ) {
+                                        duplicated = true
+                                        eachClassesByCondition[0] = param[0]
+                                        break
+                                    }
+                                }
+            
+                                if (!duplicated) {
+                                    classesConditions.push(param as any)
+                                }
+                            }
+                        } else {
+                            const keys = Object.keys(param)
+                            if (keys.length) {
+                                switch (typeof param[keys[0]]) {
+                                    case 'object':
+                                        handledParams.push(param)
+            
+                                        for (const eachProp of keys) {
+                                            const newClassesByPropValue = param[eachProp] as Record<string, string>
+                                            if (eachProp in valuesByProp) {
+                                                const classesByPropValue = valuesByProp[eachProp]
+                                                for (const eachPropValue in newClassesByPropValue) {
+                                                    classesByPropValue[eachPropValue] = newClassesByPropValue[eachPropValue]
+                                                }
+                                            } else {
+                                                valuesByProp[eachProp] = newClassesByPropValue
+                                            }
+                                        }
+                                        break
+                                } 
+                            } else {
+                                handledParams.push(param)
+                            }
+                        }
+                    }
+                    break
+                case 'function':
+                    // eslint-disable-next-line no-case-declarations
+                    const transformedParam = param(mergedValueByProp)
+                    if (typeof transformedParam === 'object') {
+                        handledParams.push(param)
+                        handleParam(transformedParam)
+                    }
+                    break
+            }
+        }
+
+        if (Array.isArray(firstParam) && 'raw' in firstParam) {
+            handleParam(firstParam)
+        }
+        for (const eachParam of params) {
+            handleParam(eachParam)
+        }
+    
+        const classNames = [
+            line(
+                handledParams.includes(firstParam)
+                    ? ''
+                    : firstParam, 
+                ...params.map(eachParam => handledParams.includes(eachParam)
+                    ? ''
+                    : typeof eachParam === 'function'
+                        ? eachParam(mergedValueByProp) || ''
+                        : eachParam)
+            )
+        ]
+        for (const eachClassesByCondition of classesConditions) {
+            if (
+                Object.entries(eachClassesByCondition[1]).every(([prop, value]) => {
+                    const propValue = mergedValueByProp[prop] ?? mergedValueByProp['$' + prop]
+                    return value === propValue || value === false && propValue === undefined
+                })
+            ) {
+                classNames.push(eachClassesByCondition[0])
+            }
+        }
+    
+        for (const eachProp in valuesByProp) {
+            const value = mergedValueByProp[eachProp] ?? mergedValueByProp['$' + eachProp] ?? ''
+            const classes = valuesByProp[eachProp][value]
+            if (classes) {
+                classNames.push(classes)
+            }
+        }
+    
+        return line(classNames)
+    }
+}
+
+export default cv
---
+++ packages/class-variant/tests/index.test.ts
@@ -0,0 +1,59 @@
+import cv from '../src'
+
+test('normal function', () => {
+    const normalBtn = cv<{ intent?: string, size?: string, color: string }>(
+        'inline-flex rounded',
+        ['top:30', 'left:40'],
+        {
+            intent: {
+                primary: 'bg:blue fg:white bg:blue-55:hover',
+                secondary: 'bg:white fg:slate-30 bg:slate-90:hover',
+            },
+            size: {
+                sm: 'text:14 p:5|15',
+                md: 'text:16 p:10|25'
+            }
+        },
+        ['uppercase', { intent: 'primary', size: 'md' }],
+        ({ intent, size }) => intent && size && 'font:semibold',
+        ({ color }) => color && `color:${color}`
+    )
+     
+    normalBtn.default = {
+        intent: 'primary',
+        size: 'sm'
+    }
+
+    expect(normalBtn()).toBe('inline-flex rounded top:30 left:40 font:semibold bg:blue fg:white bg:blue-55:hover text:14 p:5|15')
+    expect(normalBtn({ intent: 'secondary', size: 'sm', color: 'red' })).toBe('inline-flex rounded top:30 left:40 font:semibold color:red bg:white fg:slate-30 bg:slate-90:hover text:14 p:5|15')
+    expect(normalBtn({ intent: 'primary', size: 'md', color: 'red' })).toBe('inline-flex rounded top:30 left:40 font:semibold color:red uppercase bg:blue fg:white bg:blue-55:hover text:16 p:10|25')
+})
+
+test('literal function', () => {
+    const literalBtn = cv<{ intent?: string, size?: string, color: string }>`
+        inline-flex rounded
+        ${['top:30', 'left:40']}
+        ${{
+            intent: {
+                primary: 'bg:blue fg:white bg:blue-55:hover',
+                secondary: 'bg:white fg:slate-30 bg:slate-90:hover',
+            },
+            size: {
+                sm: 'text:14 p:5|15',
+                md: 'text:16 p:10|25'
+            }
+        }}
+        ${['uppercase', { intent: 'primary', size: 'md' }]}
+        ${({ intent, size }) => intent && size && 'font:semibold'}
+        color:${({ color }) => color}
+    `
+     
+    literalBtn.default = {
+        intent: 'primary',
+        size: 'sm'
+    }
+
+    expect(literalBtn()).toBe('inline-flex rounded top:30 left:40 font:semibold color: bg:blue fg:white bg:blue-55:hover text:14 p:5|15')
+    expect(literalBtn({ intent: 'secondary', size: 'sm', color: 'red' })).toBe('inline-flex rounded top:30 left:40 font:semibold color:red bg:white fg:slate-30 bg:slate-90:hover text:14 p:5|15')
+    expect(literalBtn({ intent: 'primary', size: 'md', color: 'red' })).toBe('inline-flex rounded top:30 left:40 font:semibold color:red uppercase bg:blue fg:white bg:blue-55:hover text:16 p:10|25')
+})
