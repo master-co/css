e6a669883c00cc22a096c82dba5db80a4313e365
1aron <i@aron.tw>
2023-07-02 19:38:15 +0800

    Revert "Improve(Extractor): Virtual CSS mods are prefixed with `virtual:` by default to avoid confusion with real mods #200" and prefix with `.virtual:`
    This reverts commit d608e26cdef4ef0e08c2b9d5a08ca998af5bcd58.

--- examples/react-with-static-extraction/src/main.tsx
+++ examples/react-with-static-extraction/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import 'virtual:master.css'
+import '.virtual:master.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
