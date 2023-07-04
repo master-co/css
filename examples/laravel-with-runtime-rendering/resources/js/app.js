0045720e4676981b788953f04646f831d5fa3727
Miles <swrev5741@gmail.com>
2023-07-04 22:11:02 +0800

    Update examples

---
+++ examples/laravel-with-runtime-rendering/resources/js/app.js
@@ -0,0 +1,6 @@
+import './bootstrap';
+
+import { initRuntime } from '@master/css';
+import config from './master.css';
+
+initRuntime(config);
---
+++ examples/laravel-with-runtime-rendering/resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+    </head>
+    <body>
+        <h1 class="font:40 font:heavy italic m:50 text:center"> 
+            Hello World 
+        </h1>
+    </body>
+</html>
