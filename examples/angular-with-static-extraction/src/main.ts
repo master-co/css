dee75a4ee97fe3b3d1892a3f02e5c8049c276feb
1aron <i@aron.tw>
2023-07-08 07:30:30 +0800

    Update examples

--- examples/angular-with-static-extraction/src/main.ts
+++ examples/angular-with-static-extraction/src/main.ts
@@ -1,7 +1,5 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
+import { AppModule } from './app/app.module'
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+    .catch(err => console.error(err))
