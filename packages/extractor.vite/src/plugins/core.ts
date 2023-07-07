865149cdec6d6abe1f88d3f4cf315d57ccd491da
1aron <i@aron.tw>
2023-07-07 15:15:07 +0800

    Fix(Extractor Vite): Vue/React environments don't have styles injected after server connection

--- packages/extractor.vite/src/plugins/core.ts
+++ packages/extractor.vite/src/plugins/core.ts
@@ -3,7 +3,6 @@ import type { Options } from '@master/css-extractor'
 import type { Plugin } from 'vite'
 import { Pattern } from 'fast-glob'
 import PreInsertionPlugin from './pre-insertion'
-import HMRPlugin from './hmr-plugin'
 import VirtualCSSModulePlugins from './virtual-css-module'
 
 export default function CSSExtractorPlugin(
@@ -15,7 +14,6 @@ export default function CSSExtractorPlugin(
     })
     extractor.init()
     return [
-        HMRPlugin(extractor),
         PreInsertionPlugin(extractor),
         ...VirtualCSSModulePlugins(extractor),
     ]
--- packages/extractor.vite/src/plugins/pre-insertion.ts
+++ packages/extractor.vite/src/plugins/pre-insertion.ts
@@ -1,5 +1,7 @@
 import CSSExtractor from '@master/css-extractor'
-import type { Plugin } from 'vite'
+import type { Plugin, ViteDevServer } from 'vite'
+import { readFileSync } from 'fs'
+import debounce from 'lodash.debounce'
 
 /**
  * Pre-insert code for all modules
@@ -7,6 +9,23 @@ import type { Plugin } from 'vite'
 export default function PreInsertionPlugin(
     extractor: CSSExtractor,
 ): Plugin {
+    let server: ViteDevServer
+    let transformedIndexHTMLModule: { id: string, code: string }
+    let readyForHMR = false
+    const straightUpdateVirtualModule = () => {
+        const virtualCSSModule = server.moduleGraph.getModuleById(extractor.resolvedVirtualModuleId)
+        if (virtualCSSModule) {
+            server.reloadModule(virtualCSSModule)
+        }
+    }
+    const debounceUpdateVirtualModule = debounce(straightUpdateVirtualModule, 100)
+    const updateVirtualModule = () => {
+        if (readyForHMR) {
+            straightUpdateVirtualModule()
+        } else {
+            debounceUpdateVirtualModule()
+        }
+    }
     return {
         name: 'master-css-extractor:pre-insertion',
         enforce: 'pre',
@@ -19,13 +38,87 @@ export default function PreInsertionPlugin(
         transformIndexHtml: {
             order: 'pre',
             handler: async (html, { filename }) => {
+                transformedIndexHTMLModule = {
+                    id: filename,
+                    code: html
+                }
                 await extractor.insert(filename, html)
+
             }
         },
         async transform(code, id) {
-            if (id !== extractor.resolvedVirtualModuleId) {
-                await extractor.insert(id, code)
+            const resolvedVirtualModuleId = extractor.resolvedVirtualModuleId
+            if (id !== resolvedVirtualModuleId) {
+                if (await extractor.insert(id, code)) {
+                    if (server) {
+                        updateVirtualModule()
+                    }
+                }
             }
+        },
+        configureServer(devServer) {
+            const resetHandler = async () => {
+                const tasks = []
+                /* 1. fixed sources */
+                tasks.push(await extractor.prepare())
+                /* 2. transform index.html */
+                if (transformedIndexHTMLModule) {
+                    tasks.push(extractor.insert(transformedIndexHTMLModule.id, transformedIndexHTMLModule.code))
+                }
+                /* 3. transformed modules */
+                tasks.concat(
+                    Array.from(server.moduleGraph.idToModuleMap.keys())
+                        .filter((eachModuleId) => eachModuleId !== extractor.resolvedVirtualModuleId)
+                        .map(async (eachModuleId: string) => {
+                            const eachModule = server.moduleGraph.idToModuleMap.get(eachModuleId)
+                            let eachModuleCode = eachModule.transformResult?.code
+                            if (!eachModuleCode) {
+                                eachModuleCode = readFileSync(eachModuleId, 'utf-8')
+                            }
+                            await extractor.insert(eachModuleId, eachModuleCode)
+                        })
+                )
+                await Promise.all(tasks)
+            }
+            const sendCustomHMREvent = () => {
+                const resolvedVirtualModuleId = extractor.resolvedVirtualModuleId
+                server.ws.send({
+                    type: 'update',
+                    updates: [{
+                        type: 'js-update',
+                        path: resolvedVirtualModuleId,
+                        acceptedPath: resolvedVirtualModuleId,
+                        timestamp: +Date.now()
+                    }]
+                })
+                server.ws.send({
+                    type: 'custom',
+                    event: `master-css-hmr:update`,
+                    data: {
+                        id: resolvedVirtualModuleId,
+                        css: extractor.css.text
+                    }
+                })
+            }
+            server = devServer
+            server.ws.on('connection', () => {
+                extractor
+                    .on('reset', resetHandler)
+                    .on('change', sendCustomHMREvent)
+                readyForHMR = true
+            })
+            server.ws.on('close', () => {
+                extractor
+                    .off('reset', resetHandler)
+                    .off('change', sendCustomHMREvent)
+                readyForHMR = false
+            })
+            server.ws.on('master-css-hmr:update', ({ id }) => {
+                const resolvedVirtualModuleId = extractor.resolvedVirtualModuleId
+                if (id !== resolvedVirtualModuleId) return
+                updateVirtualModule()
+            })
+            extractor.startWatch()
         },
     } as Plugin
 }
