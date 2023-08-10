2b9ca7a1b4b26aa12ad1554e9ac6d7d192113227
1aron <i@aron.tw>
2023-08-10 12:30:27 +0800

    Fix(CSS React): Client and server components shouldn't be bundled together, fixed #262

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,3 +1,7 @@
-export * from './CSSProvider'
-export * from './ThemeServiceProvider'
-export * from './styled'
+/**
+ * Don't use * to export everything from this file.
+ * @error Unsupported Server Component type: undefined
+ */
+export { CSSProvider, CSSContext, useCSS } from './CSSProvider'
+export { ThemeServiceProvider, ThemeServiceContext, useThemeService } from './ThemeServiceProvider'
+export { styled } from './styled'
