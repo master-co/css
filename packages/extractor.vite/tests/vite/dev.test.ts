f5aeb49f74bff08d9c849adbafedaac5a531bb2b
1aron <i@aron.tw>
2023-08-16 03:17:47 +0800

    Update tests

--- packages/extractor.vite/tests/vite/dev.test.ts
+++ packages/extractor.vite/tests/vite/dev.test.ts
@@ -53,8 +53,8 @@ it('change class names and check result in the browser during HMR', async () => 
     const newClassName = 'font:' + new Date().getTime()
     const newClassNameSelector = '.' + cssEscape(newClassName)
     fs.writeFileSync(templatePath, templateContent.replace('class="card"', `class="${newClassName}"`))
-    await page.waitForNetworkIdle()
     const newClassNameElementHandle = await page.waitForSelector(newClassNameSelector)
+    await page.waitForNetworkIdle()
     expect(newClassNameElementHandle).not.toBeNull()
     const styleHandle = await page.$('[data-vite-dev-id$=".virtual/master.css"]')
     expect(styleHandle).not.toBeNull()
@@ -66,8 +66,8 @@ it('change master.css.ts and check result in the browser during HMR', async () =
     const newBtnClassName = 'btn' + new Date().getTime()
     const newBtnClassNameSelector = '.' + cssEscape(newBtnClassName)
     fs.writeFileSync(templatePath, templateContent.replace('class="card"', `class="${newBtnClassName}"`))
-    await page.waitForNetworkIdle()
     const newClassNameElementHandle = await page.waitForSelector(newBtnClassNameSelector)
+    await page.waitForNetworkIdle()
     expect(newClassNameElementHandle).not.toBeNull()
     // -> classes: { btn43848384: 'xxx' }
     fs.writeFileSync(masterCSSConfigPath, `
