075156ec67d38dd8bb1840170fb376e3d92921b8
1aron <i@aron.tw>
2023-09-26 16:08:48 +0800

    Update docs

--- docs/transition/syntaxes.ts
+++ docs/transition/syntaxes.ts
@@ -1,10 +1,5 @@
 const syntaxes = [
-    ['transition:`name`|`duration`', '~`name`|`duration`'],
-    ['transition:`name`|`duration`,`…`'],
-    ['~:`name`|`duration`,`…`'],
-    ['transition:`name`|`duration`|`delay`', '~`name`|`duration`|`delay`'],
-    ['transition:`name`|`duration`|`timing-function`', '~`name`|`duration`|`timing-function`'],
-    ['transition:`name`|`duration`|`timing-function`|`delay`', '~`name`|`duration`|`timing-function`|`delay`'],
+    ['transition:`name`|`duration`,`…`', '~`name`|`duration`'],
 ]
 
 export default syntaxes
