ac653633246a31040f70c7a588a0590ee806803a
1aron <i@aron.tw>
2023-10-12 19:18:41 +0800

    Docs: Add **Master CSS Code Linting for Svelte**

---
+++ docs/code-linting/svelte/metadata.ts
@@ -0,0 +1,12 @@
+import { Metadata } from 'shared/types/Metadata'
+
+const metadata: Metadata = {
+    title: 'Install Master CSS ESLint in Svelte',
+    description: 'Guide to installing Master CSS ESLint in your Svelte project.',
+    category: 'Code Linting',
+    ogImageTitle: 'Master CSS ESLint',
+    ogImageIcon: 'shared/images/frameworks/svelte.svg',
+    ogImageIconWidth: 192
+}
+
+export default metadata
---
+++ docs/code-linting/svelte/page.tsx
@@ -0,0 +1,24 @@
+import Layout from '~/layouts/doc'
+import metadata from './metadata'
+import Content from './content.mdx'
+import { generate } from '~/utils/metadata'
+import LogoSvg from 'shared/images/frameworks/svelte.svg'
+import { queryDictionary } from 'shared/dictionaries'
+
+export async function generateMetadata(props: any, parent: any) {
+    return generate(metadata, props, parent)
+}
+
+export default async function Page(props: any) {
+    const $ = await queryDictionary(props.params.locale)
+    return (
+        /* @ts-expect-error server component */
+        <Layout {...props}
+            metadata={metadata}
+            backOnClickCategory='/docs/code-linting'
+            icon={{ Element: LogoSvg, class: 'w:64' }}
+        >
+            <Content />
+        </Layout >
+    )
+}
