80f2bc2b645921bf3011aa9178496fc96300c23a
1aron <i@aron.tw>
2023-10-19 01:01:43 +0800

    Docs: Add esm.sh CDN link for importing @master/css #230

---
+++ docs/installation/esm-sh/layout.tsx
@@ -0,0 +1,25 @@
+import Tabs, { Tab, TabBadge } from 'shared/components/Tabs'
+import { queryDictionary } from 'shared/dictionaries';
+import DocLayout from '~/layouts/doc'
+import LogoSvg from 'shared/images/cdns/esm-sh.svg'
+import metadata from './metadata';
+
+export default async function Layout(props: any) {
+    const $ = await queryDictionary(props.params.locale)
+    return (
+        <DocLayout {...props}
+            metadata={{
+                title: 'Launch Master CSS using esm.sh',
+                description: metadata.description,
+                category: 'CDN'
+            }}
+            backOnClickCategory='/docs/installation/cdn'
+            icon={{
+                Element: LogoSvg,
+                class: 'w:64'
+            }}
+        >
+            {props.children}
+        </DocLayout >
+    )
+}
---
+++ docs/installation/esm-sh/metadata.ts
@@ -0,0 +1,12 @@
+import { Metadata } from 'shared/types/Metadata'
+
+const metadata: Metadata = {
+    title: 'Runtime with esm.sh CDN',
+    description: 'Paste the esm.sh CDN to instantly launch the Master CSS runtime engine.',
+    category: 'Installation',
+    ogImageTitle: 'CDN Runtime',
+    ogImageIcon: 'shared/images/cdns/esm-sh.svg',
+    ogImageIconWidth: 164
+}
+
+export default metadata
