20140825bc7d2237e926c968d9fae1bc70238d5b
1aron <i@aron.tw>
2023-10-23 22:52:29 +0800

    Docs: Add [Install Master CSS ESLint with HTML](https://beta.css.master.co/docs/code-linting/html)

---
+++ docs/code-linting/html/metadata.ts
@@ -0,0 +1,12 @@
+import { Metadata } from 'shared/types/Metadata'
+
+const metadata: Metadata = {
+    title: 'Install Master CSS ESLint with HTML',
+    description: 'Guide to installing Master CSS ESLint in your HTML project.',
+    category: 'Code Linting',
+    ogImageTitle: 'Master CSS ESLint',
+    ogImageIcon: 'shared/icons/html.svg',
+    ogImageIconWidth: 192
+}
+
+export default metadata
