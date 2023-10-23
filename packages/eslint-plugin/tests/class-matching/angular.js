7efd272af17e96c32d2925e82514ea73fea34210
1aron <i@aron.tw>
2023-10-23 22:15:14 +0800

    Update tests

---
+++ packages/eslint-plugin/tests/class-matching/angular.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const rule = require('../../lib/rules/class-order')
+const RuleTester = require('eslint').RuleTester
+
+new RuleTester({
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+    }
+}).run('class order', rule, {
+    valid: [],
+    invalid: [
+        {
+            code: `
+                    <div class="
+                        m:8
+                        bg:black
+                        p:8
+                        fg:white
+                        f:24
+                    ">
+                        :)
+                    </div>`,
+            output: `
+                    <div class="
+                        bg:black
+                        fg:white
+                        f:24
+                        m:8
+                        p:8
+                    ">
+                        :)
+                    </div>`,
+            errors: [{ messageId: 'invalidClassOrder' }],
+            parser: require.resolve('@angular-eslint/template-parser'),
+        }
+    ],
+})
--- packages/eslint-plugin/tests/order.js
+++ packages/eslint-plugin/tests/order.js
@@ -2,15 +2,14 @@
 
 const rule = require('../lib/rules/class-order')
 const RuleTester = require('eslint').RuleTester
-const ruleTester = new RuleTester({
+
+new RuleTester({
     parserOptions: {
         ecmaFeatures: {
             jsx: true,
         },
     }
-})
-
-ruleTester.run('class order', rule, {
+}).run('class order', rule, {
     valid: [
         {
             code: `<div class="bg:black fg:white f:24 m:8 p:8">Simple, basic</div>`,
@@ -32,30 +31,6 @@ ruleTester.run('class order', rule, {
             code: `<div class="fg:white f:24 m:8 p:8 bg:black">Classnames will be ordered</div>`,
             output: `<div class="bg:black fg:white f:24 m:8 p:8">Classnames will be ordered</div>`,
             errors: [{ messageId: 'invalidClassOrder' }],
-        },
-        {
-            code: `
-                    <div class="
-                        m:8
-                        bg:black
-                        p:8
-                        fg:white
-                        f:24
-                    ">
-                        :)
-                    </div>`,
-            output: `
-                    <div class="
-                        bg:black
-                        fg:white
-                        f:24
-                        m:8
-                        p:8
-                    ">
-                        :)
-                    </div>`,
-            errors: [{ messageId: 'invalidClassOrder' }],
-            parser: require.resolve('@angular-eslint/template-parser'),
         },
         {
             code: `<div class="flex uppercase m:0 m:0>li text-decoration:none>li>a px:4>li align-items:baseline fg:gray-30>li>a gap-x:28 font:12 font:semibold pb:6>li pt:20 pt:10>li {bb:3|solid|black}>li:has(>.router-link-active) {fg:black}>li:has(>.router-link-active)>a fg:gray-10>li>a:hover box-shadow:none>li>a:focus">Group</div>`,
