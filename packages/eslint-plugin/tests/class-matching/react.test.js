405c579ceb4d250811260364db9db9719c880ff7
1aron <i@aron.tw>
2023-10-25 13:23:39 +0800

    Update tests

--- packages/eslint-plugin/tests/class-matching/react.test.js
+++ packages/eslint-plugin/tests/class-matching/react.test.js
@@ -1,70 +1,32 @@
 
-const rule = require('../../lib/rules/class-order')
+const rule = require('../../lib/rules/class-validation')
 const RuleTester = require('eslint').RuleTester
-const ruleTester = new RuleTester({
+
+new RuleTester({
     parserOptions: {
         ecmaFeatures: {
-            jsx: true,
-        },
-    }
-})
-
-ruleTester.run('class order', rule, {
+            jsx: true
+        }
+    },
+    parser: require.resolve('@typescript-eslint/parser')
+}).run('class matching react', rule, {
     valid: [
-        { code: '<h1 className={"bg:black fg:white f:24 m:8 p:8"}>Welcome {name}</h1>' }
+        { code: '<h1 className={"bg:black"}>Welcome {name}</h1>' }
     ],
     invalid: [
+        { code: '<h1 className={"bg:error"}>Welcome {name}</h1>', errors: [{ messageId: 'invalidClass' }] },
         {
             code: `
-                    export interface FakePropsInterface {
-                        readonly name?: string;
-                    }
-                    function Fake({
-                        name = 'yolo'
-                    }: FakeProps) {
-                        return (
-                        <>
-                            <h1 className={"m:8 bg:black p:8 fg:white f:24"}>Welcome {name}</h1>
-                            <p>Bye {name}</p>
-                        </>
-                        );
-                    }
-                    export default Fake;
-                `,
-            output: `
-                    export interface FakePropsInterface {
-                        readonly name?: string;
-                    }
-                    function Fake({
-                        name = 'yolo'
-                    }: FakeProps) {
-                        return (
-                        <>
-                            <h1 className={"bg:black fg:white f:24 m:8 p:8"}>Welcome {name}</h1>
-                            <p>Bye {name}</p>
-                        </>
-                        );
-                    }
-                    export default Fake;
-                `,
-            parser: require.resolve('@typescript-eslint/parser'),
-            errors: [{ messageId: 'invalidClassOrder' }],
+                import React from 'react'
+                import { styled } from '@master/css.react'
+
+                const H1 = styled.h1\`text-align:cente\`
+
+                export default () => (
+                    <H1>Hello World</H1>
+                )
+            `,
+            errors: [{ messageId: 'invalidClass' }],
         },
-        // styled
-        // {
-        //     code: `
-        //         import React from 'react'
-        //         import { styled } from '@master/css.react'
-
-        //         const H1 = styled.h1\`text-align:cente\`
-
-        //         export default () => (
-        //             <H1>Hello World</H1>
-        //         )
-        //     `,
-        //     filename: 'test.tsx',
-        //     parser: require.resolve('@typescript-eslint/parser'),
-        //     errors: [{ messageId: 'invalidClassOrder' }],
-        // },
     ]
 })
