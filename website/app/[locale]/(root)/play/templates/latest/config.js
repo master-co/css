b3fd8cd6077543a3e99385742524ce91868cc110
1aron <i@aron.tw>
2023-11-06 13:15:41 +0800

    Upgrade(Play): Forgot to upgrade configuration variables

--- website/app/[locale]/(root)/play/templates/latest/config.js
+++ website/app/[locale]/(root)/play/templates/latest/config.js
@@ -9,15 +9,15 @@ export default {
     },
     variables: {
         primary: {
-            '': 'gold-75@light gold-80@dark',
-            filled: 'gold-75@light gold-70@dark'
+            '': { '@light': '$(gold-75)', '@dark': '$(gold-80)' },
+            filled: { '@light': '$(gold-75)', '@dark': '$(gold-70)' }
         },
-        major: 'slate-10@light gray-80@dark',
-        neutural: 'slate-30@light gray-60@dark',
-        panel: 'white@light gray-25@dark',
-        divider: 'slate-60/.2@light white/.1@dark',
-        ring: 'slate-60/.1@light white/.1@dark',
-        shadow: 'slate-70@light black@dark',
+        major: { '@light': '$(slate-10)', '@dark': '$(gray-80)' },
+        neutural: { '@light': '$(slate-30)', '@dark': '$(gray-60)' },
+        panel: { '@light': '$(white)', '@dark': '$(gray-25)' },
+        divider: { '@light': '$(slate-60)/.2', '@dark': '$(white)/.1' },
+        ring: { '@light': '$(slate-60)/.1', '@dark': '$(white)/.1' },
+        shadow: { '@light': '$(slate-70)', '@dark': '$(black)' },
         boxShadow: {
             x3: '0|2|4|shadow/.12,0|4|8|shadow/.08,0|20|30|shadow/.1'
         }
