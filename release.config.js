3acee8245efd061af50ec8fd3c9d8d1029c1ffad
1aron <i@aron.tw>
2023-11-14 14:19:15 +0800

    Update release assets

--- release.config.js
+++ release.config.js
@@ -3,7 +3,12 @@ const configure = require('semantic-release-config-techor/configure')
 module.exports = configure({
     assets: [
         {
-            path: 'packages/css/dist/index.browser.bundle.js',
+            path: 'packages/css/dist/runtime.browser.bundle.js',
+            name: 'runtime-css.js',
+            label: 'runtime-css.js'
+        },
+        {
+            path: 'packages/css/dist/index.bundle.js',
             name: 'master-css.js',
             label: 'master-css.js'
         },
