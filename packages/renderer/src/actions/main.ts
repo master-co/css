98d8229a6f1318fae280ef940aff1e909f6b587b
1aron <i@aron.tw>
2023-11-14 23:10:43 +0800

    Update renderer deps

--- packages/renderer/src/actions/main.ts
+++ packages/renderer/src/actions/main.ts
@@ -1,22 +1,18 @@
-
 import log from '@techor/log'
 import type { Pattern } from 'fast-glob'
-import { render } from '@master/css-server'
-import zlib from 'zlib'
-import fs from 'fs'
-import prettyBytes from 'pretty-bytes'
 import prettyHartime from 'pretty-hrtime'
-import exploreConfig from 'explore-config'
 import { explorePathsSync } from '@techor/glob'
+import exploreConfig from 'explore-config'
+import { readFile, writeFile, writeFileSync } from 'fs'
+import { brotliCompressSync } from 'zlib'
+import bytes from 'bytes'
+import { render } from '@master/css-server'
 
 module.exports = async function action(filePatterns: Pattern | Pattern[], options: any = {
     config: 'master.css.*'
 }) {
     const sourcePaths = explorePathsSync(filePatterns)
     if (sourcePaths.length) {
-        const config = typeof options.config === 'string'
-            ? exploreConfig(options.config)
-            : undefined
         const renderStart = process.hrtime()
         const col1Width = Math.max(
             sourcePaths.reduce((max: any, current: any) => {
@@ -30,26 +26,32 @@ module.exports = async function action(filePatterns: Pattern | Pattern[], option
             20
         )
         const col2Width = 8
+        const config = typeof options.config === 'string'
+            ? exploreConfig(options.config)
+            : undefined
         log``
         log`${'  Source Files'.padEnd(col1Width)}${'CSS Size'.padStart(col2Width)}`
         await Promise.all(sourcePaths
-            .map(async (eachSourcePath) => {
-                const content = fs.readFileSync(eachSourcePath, { encoding: 'utf-8' })
-                const { html, css } = render(content, config)
-                const renderedCSSText = css?.text
-                const renderedCSSSize = renderedCSSText
-                    ? (options.analyze
-                        ? zlib.brotliCompressSync(renderedCSSText).length
-                        : renderedCSSText.length)
-                    : 0
-                const prettifiedCSSSize = prettyBytes(renderedCSSSize, { space: false })
-                if (!options.analyze) {
-                    if (content !== html) {
-                        fs.writeFileSync(eachSourcePath, html)
+            .map(async (filename) => new Promise<void>((resolve, reject) => {
+                readFile(filename, { encoding: 'utf-8' }, (err, content) => {
+                    if (err) reject(err)
+                    const { html, css } = render(content, config)
+                    const renderedCSSText = css?.text
+                    const renderedCSSSize = renderedCSSText
+                        ? (options.analyze
+                            ? brotliCompressSync(renderedCSSText).length
+                            : renderedCSSText.length)
+                        : 0
+                    if (!options.analyze) {
+                        if (content !== html) {
+                            writeFileSync(filename, html)
+                        }
                     }
-                }
-                log.ok`**${eachSourcePath}**${' '.repeat(col1Width - eachSourcePath.length - 2)}${prettifiedCSSSize.padStart(col2Width)}`
-            }))
+                    const prettifiedCSSSize = bytes(renderedCSSSize, { space: false })
+                    log.ok`**${filename}**${' '.repeat(col1Width - filename.length - 2)}${prettifiedCSSSize.padStart(col2Width)}`
+                    resolve()
+                })
+            })))
         const renderTime = process.hrtime(renderStart)
         if (options.analyze) {
             log`${' '.repeat(col1Width)}${'(Brotli)'.padStart(col2Width)}`
