bb446d512d35742d5916c0874ef484a134e9490b
1aron <i@aron.tw>
2023-12-15 21:13:43 +0800

    Docs: New documentation **Development strategies for minimizing CSS**

---
+++ website/app/[locale]/(root)/docs/minimization/metadata.ts
@@ -0,0 +1,13 @@
+import { Metadata } from 'websites/types/Metadata'
+
+const metadata: Metadata = {
+    title: 'Development strategies for minimizing CSS',
+    description: 'Reduce on-page CSS output and complexity using Master CSS APIs.',
+    category: 'Production Optimization',
+    unfinished: true,
+    other: {
+        subject: 'Minimization'
+    }
+}
+
+export default metadata
