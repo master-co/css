be7c55689e76af601ec26955bce3b660382b5805
1aron <i@aron.tw>
2023-12-20 00:00:38 +0800

    Update category in metadata.ts

--- website/app/[locale]/play/metadata.ts
+++ website/app/[locale]/play/metadata.ts
@@ -1,9 +1,10 @@
 import { Metadata } from 'websites/types/Metadata'
+import version from '~/version'
 
 const metadata: Metadata = {
     title: 'Master CSS Play',
     description: 'A real-time code editor for Master CSS.',
-    category: 'Online',
+    category: `v${version}`,
     openGraph: {
         title: 'Playground'
     }
