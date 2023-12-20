ddc86310616ea3c7eeded9d56c94166a177bf46e
1aron <i@aron.tw>
2023-12-20 22:20:19 +0800

    Docs: Add **Screen Sizes** documentation

---
+++ website/app/[locale]/(root)/docs/screen-sizes/metadata.ts
@@ -0,0 +1,9 @@
+import { Metadata } from 'websites/types/Metadata'
+
+const metadata: Metadata = {
+    title: 'Screen Sizes',
+    description: 'The screen sizes are derived from common display resolutions.',
+    category: 'Design Tokens'
+}
+
+export default metadata
