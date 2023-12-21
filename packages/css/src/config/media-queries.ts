dd9dbbbb83c37e497b47b06ca0ea9a017cf38cc9
1aron <i@aron.tw>
2023-12-21 20:54:44 +0800

    Improve: Refactor media queries to use screen sizes tokens

--- packages/css/src/config/media-queries.ts
+++ packages/css/src/config/media-queries.ts
@@ -1,15 +1,1 @@
-const mediaQueries = {
-    '4xs': 360,
-    '3xs': 480,
-    '2xs': 600,
-    'xs': 768,
-    'sm': 834,
-    'md': 1024,
-    'lg': 1280,
-    'xl': 1440,
-    '2xl': 1600,
-    '3xl': 1920,
-    '4xl': 2560
-}
-
-export default mediaQueries
+export { default } from '../tokens/screen-sizes'
