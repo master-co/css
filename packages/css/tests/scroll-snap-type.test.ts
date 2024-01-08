79859a04f707f53afb8f6695fce7942db720621f
1aron <i@aron.tw>
2024-01-08 21:34:32 +0800

    Update scroll-snap-type tests and syntaxes

--- packages/css/tests/scroll-snap-type.test.ts
+++ packages/css/tests/scroll-snap-type.test.ts
@@ -1,4 +1,5 @@
 test('scroll-snap-type', () => {
     expect(new MasterCSS().create('scroll-snap:x')?.text).toContain('scroll-snap-type:x')
     expect(new MasterCSS().create('scroll-snap-type:x')?.text).toContain('scroll-snap-type:x')
+    expect(new MasterCSS().create('scroll-snap-type:x|mandatory')?.text).toContain('scroll-snap-type:x mandatory')
 })
--- website/app/[locale]/(root)/docs/scroll-snap-type/syntaxes.ts
+++ website/app/[locale]/(root)/docs/scroll-snap-type/syntaxes.ts
@@ -1,14 +1,13 @@
 const syntaxes = [
-    ['scroll-snap-type:`value`', 'scroll-snap:`value`'],
-    ['scroll-snap-type:`value`|`value`', 'scroll-snap:`value`|value`'],
+    ['scroll-snap-type:`value`'],
     ['scroll-snap:x'],
     ['scroll-snap:y'],
     ['scroll-snap:both'],
     ['scroll-snap:block'],
     ['scroll-snap:inline'],
-    ['scroll-snap:x|mandatory'],
-    ['scroll-snap:y|proximity'],
-    ['scroll-snap:both|mandatory'],
+    ['scroll-snap-type:x|mandatory'],
+    ['scroll-snap-type:y|proximity'],
+    ['scroll-snap-type:both|mandatory'],
     ['scroll-snap-type:none'],
 ]
 
