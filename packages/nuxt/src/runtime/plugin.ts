80d30433d362bd4dcfb57308d8d268ab8f4ec4ce
1aron <i@aron.tw>
2024-01-15 12:31:25 +0800

    Update dependencies and fix TypeScript errors in plugin.ts

--- packages/nuxt/src/runtime/plugin.ts
+++ packages/nuxt/src/runtime/plugin.ts
@@ -1,13 +1,13 @@
 import { render } from '@master/css-server'
 import type { NitroApp } from 'nitropack'
 import exploreConfig from 'explore-config'
-// @ts-ignore
+// @ts-expect-error virtual module
 import pluginOptions from '#master-css-nuxt-plugin-options'
-// @ts-ignore
+// @ts-expect-error virtual module
 import nuxtOptions from '#nuxt-options'
 
 export default ((nitro: NitroApp) => {
-    const config = exploreConfig['default'](pluginOptions.config ?? 'master.css.*', { cwd: nuxtOptions.srcDir })
+    const config = exploreConfig(pluginOptions.config ?? 'master.css.*', { cwd: nuxtOptions.srcDir })
     nitro.hooks.hook('render:response', async (response) => {
         if (typeof response.body === 'string' && (response.headers?.['Content-Type'] || response.headers?.['content-type'])?.includes('html')) {
             const { html } = render(response.body, config)
