583510d85309ff5a9a18e34975c09c8fed25996b
1aron <i@aron.tw>
2024-01-15 17:19:57 +0800

    Update roadmap metadata date

--- website/app/[locale]/(root)/roadmap/metadata.ts
+++ website/app/[locale]/(root)/roadmap/metadata.ts
@@ -3,7 +3,7 @@ import { Metadata } from 'websites/types/Metadata'
 const metadata: Metadata = {
     title: 'Roadmap',
     description: 'Currently in release candidate and moving towards stable.',
-    date: '2024-01-10T17:18:26.853Z',
+    date: '2024-01-15T09:19:21.050Z',
     category: 'v2.0'
 }
 
