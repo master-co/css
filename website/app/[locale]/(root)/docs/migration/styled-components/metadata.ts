59527e692117061762ef5123a94b8e37a16acf38
1aron <i@aron.tw>
2024-01-17 17:45:11 +0800

    Refactor styled-components to use Master CSS

---
+++ website/app/[locale]/(root)/docs/migration/styled-components/metadata.ts
@@ -0,0 +1,11 @@
+import { Metadata } from 'websites/types/Metadata'
+
+const metadata: Metadata = {
+    title: 'Migrating from Styled Components',
+    description: 'A guide on how to refactor Styled Components using Master CSS.',
+    category: 'Migration',
+    vercelOG: true,
+    unfinished: true
+}
+
+export default metadata
---
+++ website/app/[locale]/(root)/docs/migration/styled-components/page.tsx
@@ -0,0 +1,20 @@
+import Layout from '~/layouts/doc'
+import metadata from './metadata'
+/* @ts-expect-error toc */
+import Content, { toc } from './content.mdx'
+import { generate } from '~/utils/metadata'
+
+export const dynamic = 'force-static'
+export const revalidate = false
+
+export async function generateMetadata(props: any, parent: any) {
+    return generate(metadata, props, parent)
+}
+
+export default async function Page(props: any) {
+    return (
+        <Layout {...props} metadata={metadata} toc={toc} titleBig backOnClickCategory='/docs/migration'>
+            <Content />
+        </Layout >
+    )
+}
