69c044a2b6bbb51d0adabd1fd804982119dfed9b
1aron <i@aron.tw>
2024-01-21 22:15:54 +0800

    Update dependencies and add new files

--- website/app/[locale]/(root)/docs/installation/(tabs)/general/metadata.ts
+++ website/app/[locale]/(root)/docs/installation/(tabs)/general/metadata.ts
@@ -1,7 +1,7 @@
 import { Metadata } from 'websites/types/Metadata'
 
 const metadata: Metadata = {
-    title: 'Genernal Installation',
+    title: 'General Installation',
     description: 'The easiest way to use Master CSS from scratch is to initialize the runtime engine directly in the application\'s entry file.',
     category: 'Getting Started'
 }
