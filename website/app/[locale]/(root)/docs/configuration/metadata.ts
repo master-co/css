56a8561d6ee37642ad40795bf513685c5cd40fae
1aron <i@aron.tw>
2024-01-22 20:05:40 +0800

    Docs: Rename command `npx mcss init` to `npx @master/css init`

--- website/app/[locale]/(root)/docs/configuration/metadata.ts
+++ website/app/[locale]/(root)/docs/configuration/metadata.ts
@@ -1,8 +1,8 @@
 import { Metadata } from 'websites/types/Metadata'
 
 const metadata: Metadata = {
-    title: 'Configuration',
-    description: 'A guide to fully customizing your design system using the configuration API.',
+    title: 'master.css.js',
+    description: 'Implement your design system using the configuration API.',
     category: 'Configuration',
     order: 1
 }
--- website/app/[locale]/(root)/docs/configuration/page.tsx
+++ website/app/[locale]/(root)/docs/configuration/page.tsx
@@ -1,4 +1,4 @@
-import Layout from './Layout'
+import Layout from '~/layouts/reference'
 import metadata from './metadata'
 /* @ts-expect-error toc */
 import Content, { toc } from './content.mdx'
