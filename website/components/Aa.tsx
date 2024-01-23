9a62fa8f7eb760017dd941f0b28999dc9f8810f7
1aron <i@aron.tw>
2024-01-23 16:41:57 +0800

    Docs: Add **Text Colors** documentation

--- website/components/Aa.tsx
+++ website/components/Aa.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx'
 
 export default function Aa(props: any) {
-    return <span {...props} className={clsx('font:16 font:medium mr:3x v:top', props.className, { 'bg:tiny': props.className === 'fg:transparent' })}>Aa</span>
+    return <span {...props} className={clsx('font:16 font:medium mr:3x user-select:none v:top', props.className, { 'bg:tiny': props.className === 'fg:transparent' })}>Aa</span>
 }
