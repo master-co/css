f6d5ce9118b3abfb9019cb5db72d6d21eea5e77b
1aron <i@aron.tw>
2024-01-26 03:21:34 +0800

    Remove unused files and update metadata

--- website/app/[locale]/(root)/docs/introduction/metadata.ts
+++ website/app/[locale]/(root)/docs/introduction/metadata.ts
@@ -4,6 +4,7 @@ const metadata: Metadata = {
     title: 'Introduction',
     description: 'The CSS language and framework for rapidly building modern and high-performance websites.',
     category: 'Getting Started',
+    unfinished: true,
     order: 1
 }
 
