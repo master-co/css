88a049014327169c9a5baa5f3cf1cca02ba41e14
1aron <i@aron.tw>
2024-01-29 13:12:12 +0800

    Fix class attribute in DemoDark component

--- website/components/DemoDark.tsx
+++ website/components/DemoDark.tsx
@@ -1,7 +1,7 @@
 import { IconMoon } from '@tabler/icons-react'
 
 export default (props: any) =>
-    <div class="dark w:1/2">
+    <div className="dark w:1/2">
         <div className="bg-stripe dark bg:base overflow:hidden">
             <IconMoon className='abs fg:lighter font:12 right:4x top:4x' strokeWidth={1} width={24} height={24}/>
             <div className='float:left py:8x translateX(-50%)'>
