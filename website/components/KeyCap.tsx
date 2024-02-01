bf69dd2eb45d8eb5725ffc1760640d5cf9b9a1d2
1aron <i@aron.tw>
2024-02-02 03:25:06 +0800

    Update documentation and KeyCap styling

--- website/components/KeyCap.tsx
+++ website/components/KeyCap.tsx
@@ -1,3 +1,3 @@
 import { styled } from '@master/css.react'
 
-export default styled.span`bg:surface r:1x p:1x b:1|frame s:01 font:12 fg:strong`
+export default styled.span`bg:surface r:1x p:1x|2x b:1|frame s:01 font:12 fg:strong`
