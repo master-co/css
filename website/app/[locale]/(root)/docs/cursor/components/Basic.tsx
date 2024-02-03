7efe1011e25435e86c47eac99606b9171e126a2b
1aron <i@aron.tw>
2024-02-03 11:02:23 +0800

    Update CSS class names for background stripes

--- website/app/[locale]/(root)/docs/cursor/components/Basic.tsx
+++ website/app/[locale]/(root)/docs/cursor/components/Basic.tsx
@@ -7,7 +7,7 @@ export default ({ className }: any) => {
     return (
         <>
             <Demo className="gap:40">
-                <button className="bg-stripe cursor:pointer font:14 font:semibold h:42 px:20 r:5">Hover Me</button>
+                <button className="bg:stripe cursor:pointer font:14 font:semibold h:42 px:20 r:5">Hover Me</button>
             </Demo>
             <Code lang="html">{`
                 <div class="**${className}**">Hover Me</div>
--- website/app/[locale]/(root)/docs/text-align/components/Basic.tsx
+++ website/app/[locale]/(root)/docs/text-align/components/Basic.tsx
@@ -9,7 +9,7 @@ export default ({ className }: any) => <>
     <Demo $py={0}>
         <DemoPanel>
             <p className={clsx('my:0', className)}>
-                <span className='bg-stripe-pink rounded'>
+                <span className='bg:stripe-pink rounded'>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et elit dictum, tempor augue quis, rhoncus enim. Nunc lacinia, velit vel convallis tincidunt, ante nisi maximus nunc, at aliquam nisi lectus in mauris.
                 </span>
             </p>
