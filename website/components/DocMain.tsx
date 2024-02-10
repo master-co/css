76690f6de6d68210124c6d6dc93941b509f99cc6
1aron <i@aron.tw>
2024-02-10 13:01:16 +0800

    Update DocMain component styling

--- website/components/DocMain.tsx
+++ website/components/DocMain.tsx
@@ -6,6 +6,7 @@ const DocMain = styled.main(
     'max-w:screen-xl mx:auto p:60|30@print pb:5x pb:20x@md pt:35x@md px:5x',
     ({ $hideRightSide, $hideLeftSide }) => ({
         'pr:85x@lg': !$hideRightSide,
+        'max-w:screen-sm_:where(p)': $hideRightSide,
         'pl:75x@md pl:85x@lg': !$hideLeftSide,
     }),
     ({ $pt }) => `pt:${$pt || '20x'}`
