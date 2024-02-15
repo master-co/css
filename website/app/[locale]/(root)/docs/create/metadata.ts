7b00da19a5f52ed2b165bab4c54d58b70d27bd75
1aron <i@aron.tw>
2024-02-15 14:20:39 +0800

    Docs: Add new files for creating Master CSS config and content

---
+++ website/app/[locale]/(root)/docs/create/metadata.ts
@@ -0,0 +1,9 @@
+import { Metadata } from 'websites/types/Metadata'
+
+const metadata: Metadata = {
+    title: '@master/create-css',
+    description: 'CLI for creating a Master CSS config file or application.',
+    category: 'API Reference'
+}
+
+export default metadata
