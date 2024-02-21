262f6435a344720a96e8aa65ab3c44053525c9df
1aron <i@aron.tw>
2024-02-22 06:49:12 +0800

    Perf(Nuxt): Improve install size and config lookup

--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -2,25 +2,34 @@ import { defineNuxtModule, addServerPlugin, createResolver } from '@nuxt/kit'
 import { name } from '../package.json'
 import log from '@techor/log'
 import { relative } from 'path'
-import fg from 'fast-glob'
+import { existsSync } from 'fs'
 
 export default defineNuxtModule<{ config?: string }>({
     meta: {
         name,
         configKey: 'mastercss'
     },
-    setup({ config }, nuxt) {
+    setup(options = {}, nuxt) {
+        if (!options.config) options.config = 'master.css'
         if (!nuxt.options.ssr || nuxt.options._prepare) return
         const { resolve } = createResolver(import.meta.url)
-        const configFilePattern = resolve(nuxt.options.srcDir, config ?? 'master.css.*')
-        const foundConfigFile = fg.sync(configFilePattern)[0]
+        // try to find the config file with the given name and options.extensions
+        let foundConfigPath: string | undefined
+        for (const eachExtension of ['js', 'mjs', 'ts', 'cjs', 'cts', 'mts']) {
+            const eachBasename = options.config + '.' + eachExtension
+            const eachPath = resolve(nuxt.options.srcDir, eachBasename)
+            if (existsSync(eachPath)) {
+                foundConfigPath = eachPath
+                break
+            }
+        }
         nuxt.hook('nitro:config', (config) => {
-            if (foundConfigFile) {
-                log.ok`**${relative(nuxt.options.srcDir, foundConfigFile)}** config file found`
+            if (foundConfigPath) {
+                log.ok`**${relative(nuxt.options.srcDir, foundConfigPath)}** config file found`
                 config.alias ??= {}
-                config.alias['#master-css-config'] = foundConfigFile
+                config.alias['#master-css-config'] = foundConfigPath
             } else {
-                log.i`No **${relative(nuxt.options.srcDir, configFilePattern)}** config file found`
+                log.i`No **${relative(nuxt.options.srcDir, options.config || '')}** config file found`
                 // prevent nuxt from complaining about missing virtual module
                 config.virtual ??= {}
                 config.virtual['#master-css-config'] = `export default {}`
