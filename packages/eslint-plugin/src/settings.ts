bcf2629152f0691fe18d44eb2aea025259ce2033
1aron <i@aron.tw>
2024-02-22 06:51:05 +0800

    Update(ESLint): `settings.config` from `master.css.*` to `master.css`

--- packages/eslint-plugin/src/settings.ts
+++ packages/eslint-plugin/src/settings.ts
@@ -2,7 +2,7 @@ const settings = {
     calleeMatching: '^(classnames|clsx|ctl|cva|cv|classVariant|styled(?:\\.\\w+)?)',
     classMatching: '^class(Name)?$',
     ignoredKeys: ['compoundVariants', 'defaultVariants'],
-    config: 'master.css.*'
+    config: 'master.css'
 }
 
 export default settings
