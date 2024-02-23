cbd2e1c82644d82306c19c6d6d9c92653df7762e
Aron <i@aron.tw>
2024-02-23 23:14:23 +0800

    Update workspace dependencies to use current version

--- packages/create/tests/new/test.ts
+++ packages/create/tests/new/test.ts
@@ -1,10 +1,14 @@
 import { execSync } from 'node:child_process'
 import { rm } from 'css-shared/utils/fs'
 import { join } from 'node:path'
-import { existsSync } from 'node:fs'
+import { existsSync, readFileSync } from 'node:fs'
 
 it('creates a new app', () => {
     rm(join(__dirname, 'my-app'))
     execSync('tsx ../../src/bin my-app', { cwd: __dirname })
     expect(existsSync(join(__dirname, 'my-app/package.json'))).toBe(true)
+})
+
+it('should install the remote dependencies', () => {
+    expect(readFileSync(join(__dirname, 'my-app/package.json')).toString()).not.toContain('workspace:^')
 })
