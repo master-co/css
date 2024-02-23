8be1c6e91cb0db6b6a24fc1e4c09f6ef92167834
Aron <i@aron.tw>
2024-02-23 22:51:48 +0800

    Update tsconfig.json, package.json, and metadata.ts

--- website/app/[locale]/(root)/docs/introduction/metadata.ts
+++ website/app/[locale]/(root)/docs/introduction/metadata.ts
@@ -1,11 +1,17 @@
 import { Metadata } from 'websites/types/Metadata'
 
 const metadata: Metadata = {
-    title: 'Introduction',
+    title: 'The All-in-One CSS Solution for Building Modern and Performant Websites',
     description: 'Get a quick overview of Master CSS and its benefits.',
     category: 'Getting Started',
     disabled: true,
-    order: 1
+    order: 1,
+    openGraph: {
+        title: 'Introduction'
+    },
+    other: {
+        subject: 'Introduction'
+    }
 }
 
 export default metadata
