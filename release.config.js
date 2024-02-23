4ad936b74a1ddc16babbc0856bd367cbcc1c2c78
Aron <i@aron.tw>
2024-02-23 19:57:34 +0800

    Update asset paths in release.config.js

--- release.config.js
+++ release.config.js
@@ -3,19 +3,14 @@ const configure = require('semantic-release-config-techor/configure')
 module.exports = configure({
     assets: [
         {
-            path: 'packages/css/dist/index.bundle.js',
-            name: 'css-${nextRelease.gitTag}.js',
-            label: 'css-${nextRelease.gitTag}.js'
-        },
-        {
-            path: 'packages/runtime/dist/iife.bundle.js',
-            name: 'runtime-${nextRelease.gitTag}.js',
-            label: 'runtime-${nextRelease.gitTag}.js'
+            path: 'packages/runtime/dist/global.min.js',
+            name: 'global-${nextRelease.gitTag}.min.js',
+            label: 'global-${nextRelease.gitTag}.min.js'
         },
         {
             path: 'packages/normal.css/dist/index.css',
-            name: 'normal-${nextRelease.gitTag}.css',
-            label: 'normal-${nextRelease.gitTag}.css'
+            name: 'normal-${nextRelease.gitTag}.min.css',
+            label: 'normal-${nextRelease.gitTag}.min.css'
         }
     ]
 })
