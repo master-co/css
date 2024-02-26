6b1641fadc6fdfb1e46d7034543cbb25829894ca
Aron <i@aron.tw>
2024-02-26 09:59:06 +0800

    Add "type" field to package.json files

--- packages/cli/src/bin/index.ts
+++ packages/cli/src/bin/index.ts
@@ -1,25 +1,5 @@
 #!/usr/bin/env node
-import { resolve } from 'node:path'
-import { Command } from 'commander'
-import { readJSONFileSync } from '@techor/fs'
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+import runProgram from '../core'
 
-const pkg = readJSONFileSync(resolve(__dirname, '../../package.json'))
-const program = new Command()
-
-program
-    .name(pkg.name)
-    .description(pkg.description)
-    .version(pkg.version || '0.0.0')
-
-import commandExtract from '../commands/extract'
-import commandRender from '../commands/render'
-
-commandExtract(program)
-commandRender(program)
-
-program.parse()
+runProgram()
