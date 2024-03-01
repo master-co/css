1d2f87c0719df38c8740e4595df8d7007add77c2
Aron <i@aron.tw>
2024-03-01 12:43:44 +0800

    Refactor theme switcher in HelloWorld.vue

--- examples/vue.js/src/components/HelloWorld.vue
+++ examples/vue.js/src/components/HelloWorld.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
 import { ref, inject } from 'vue'
-import { styled } from '@master/css.vue'
 
 const count = ref<number>(0)
 const themeService = inject<any>('theme-service')
-const Select = styled.select`abs full inset:0 r:inherit opacity:0 font:inherit`
 
 </script>
 
@@ -27,7 +25,8 @@ const Select = styled.select`abs full inset:0 r:inherit opacity:0 font:inherit`
         <button type="button" @click="count++">count is {{ count }}</button>
         <button className="h:40 bg:gray-20@dark bg:slate-90@light ml:10 rel">
             {{ themeService.current.value === 'dark' ? '🌜' : '☀️' }} {{ themeService.value.value }}
-            <Select v-bind="themeService.value" @change="themeService.switch($event.target.value)">
+            <Select className="abs full inset:0 r:inherit opacity:0 font:inherit" v-bind="themeService.value"
+                @change="themeService.switch($event.target.value)">
                 <option value="light">☀️ Light</option>
                 <option value="dark">🌜 Dark</option>
                 <option value="system">System</option>
