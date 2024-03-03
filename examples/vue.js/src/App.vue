96fa0502ccbdbee17a53e3f72ce455c7c2f4965b
Aron <i@aron.tw>
2024-03-03 22:17:47 +0800

    Refactor Vue.js code to remove unused imports and update logo sizes

--- examples/vue.js/src/App.vue
+++ examples/vue.js/src/App.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
-import { CSSRuntimeProvider, ThemeServiceProvider } from '@master/css.vue'
+import CSSRuntimeProvider from '@master/css.vue'
 import config from '../master.css'
 import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
     <CSSRuntimeProvider :config="config">
-        <ThemeServiceProvider :options="{ default: 'system' }">
-            <HelloWorld />
-        </ThemeServiceProvider>
+        <HelloWorld />
     </CSSRuntimeProvider>
 </template>
