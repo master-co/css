1674871d508f76e62be6c5e27d0b9146066b0c4a
Aron <i@aron.tw>
2024-03-04 01:27:19 +0800

    Update imports and configurations

--- packages/svelte/playwright-ct.config.ts
+++ packages/svelte/playwright-ct.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig, devices } from '@playwright/experimental-ct-svelte'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
     testDir: './',
@@ -16,13 +15,13 @@ export default defineConfig({
             name: 'chromium',
             use: { ...devices['Desktop Chrome'] },
         },
-        // {
-        //     name: 'firefox',
-        //     use: { ...devices['Desktop Firefox'] },
-        // },
-        // {
-        //     name: 'webkit',
-        //     use: { ...devices['Desktop Safari'] },
-        // },
+        {
+            name: 'firefox',
+            use: { ...devices['Desktop Firefox'] },
+        },
+        {
+            name: 'webkit',
+            use: { ...devices['Desktop Safari'] },
+        },
     ],
 })
