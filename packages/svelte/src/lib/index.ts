21f74d55de8e331d9717321e0a9df8da54bc1ca2
Aron <i@aron.tw>
2024-03-03 22:57:06 +0800

    Add Fragment component

--- packages/svelte/src/lib/index.ts
+++ packages/svelte/src/lib/index.ts
@@ -1,3 +1,4 @@
 export { getRuntimeCSS } from './get-runtime-css'
 export { default as CSSRuntimeProvider } from './CSSRuntimeProvider.svelte'
+export { default as Fragment } from './Fragment.svelte'
 export { default } from './CSSRuntimeProvider.svelte'
