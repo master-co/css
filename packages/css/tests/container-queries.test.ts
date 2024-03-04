3eb6a7a1d10bab8e985582197b302860a4a3a7ef
Aron <i@aron.tw>
2024-03-04 15:30:41 +0800

    Fix container queries test case

--- packages/css/tests/container-queries.test.ts
+++ packages/css/tests/container-queries.test.ts
@@ -1,4 +1,4 @@
 test('container queries', () => {
     expect(new MasterCSS().add('font:32@container|sidebar(min-width:800px)').text)
-        .toBe('@container sidebar(min-width:800px){.font\\:32\\@container\\(min-width\\:800px\\){font-size:2rem}}')
+        .toBe('@container  sidebar(min-width:800px){.font\\:32\\@container\\|sidebar\\(min-width\\:800px\\){font-size:2rem}}')
 })
