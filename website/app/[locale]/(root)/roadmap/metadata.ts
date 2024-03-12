36861921ece5d6bf1e5e907d6b718cf1a6134596
Aron <i@aron.tw>
2024-03-13 00:25:33 +0800

    Update metadata and package versions

--- website/app/[locale]/(root)/roadmap/metadata.ts
+++ website/app/[locale]/(root)/roadmap/metadata.ts
@@ -4,7 +4,7 @@ import version from '~/version'
 const metadata: Metadata = {
     title: 'Roadmap',
     description: 'v2 is coming soon.',
-    date: '2024-03-12T14:16:36.232Z',
+    date: '2024-03-12T16:19:12.321Z',
     category: 'v' + version,
     authors: [{ name: 'Aron' }]
 }
