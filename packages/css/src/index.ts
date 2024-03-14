613e939912de6dba5665cb662ff03cefe1054283
Aron <i@aron.tw>
2024-03-14 18:14:19 +0800

    Add new functions for comparing rule modes, queries, selectors, and states

--- packages/css/src/index.ts
+++ packages/css/src/index.ts
@@ -6,5 +6,9 @@ export { default as MasterCSS, default } from './core'
 // functions
 export { default as extendConfig } from './functions/extend-config'
 export { default as reorderForReadableClasses } from './functions/reorder-for-readable-classes'
+export { default as areRuleModesEqual } from './functions/are-rule-modes-equal'
+export { default as areRuleQueriesEqual } from './functions/are-rule-queries-equal'
+export { default as areRuleSelectorsEqual } from './functions/are-rule-selectors-equal'
+export { default as areRuleStatesEqual } from './functions/are-rule-states-equal'
 
 export type { NativeRule, MediaFeatureComponent, MediaQuery } from './rule'
