c34b463852c6f975c12fa7cdc5cd16b613b76caf
Aron <i@aron.tw>
2024-03-14 19:26:24 +0800

    Add test for areRulesDuplicated function

---
+++ packages/css/tests/functions/are-rules-deplicated.test.ts
@@ -0,0 +1,18 @@
+import areRulesDuplicated from '../../src/functions/are-rules-duplicated'
+
+test('rules', () => {
+    expect(areRulesDuplicated(
+        new MasterCSS().generate('font:16')[0],
+        new MasterCSS().generate('font:32')[0])
+    ).toBeTruthy()
+
+    expect(areRulesDuplicated(
+        new MasterCSS().generate('text:16')[0],
+        new MasterCSS().generate('font:32')[0])
+    ).toBeFalsy()
+
+    expect(areRulesDuplicated(
+        new MasterCSS().generate('block')[0],
+        new MasterCSS().generate('display:none')[0])
+    ).toBeTruthy()
+})
