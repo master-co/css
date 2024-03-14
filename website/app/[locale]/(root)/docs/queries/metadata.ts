b4ca2a5d5a62e949296974e03341c9aedcafb915
Aron <i@aron.tw>
2024-03-14 23:24:14 +0800

    Rename Media Queries to Queries

--- website/app/[locale]/(root)/docs/queries/metadata.ts
+++ website/app/[locale]/(root)/docs/queries/metadata.ts
@@ -1,7 +1,7 @@
 import { Metadata } from 'websites/types/Metadata'
 
 const metadata: Metadata = {
-    title: 'Media Queries',
+    title: 'Queries',
     description: 'Customizing media queries and breakpoints for your design system.',
     category: 'Custom Syntax',
     unfinished: true
