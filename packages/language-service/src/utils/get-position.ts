2fcb72a547baaca54a434dd1a0c1791ec88ba654
Aron <i@aron.tw>
2024-03-18 16:49:15 +0800

    Add getPosition utility function

---
+++ packages/language-service/src/utils/get-position.ts
@@ -0,0 +1,8 @@
+import { TextDocument } from 'vscode-languageserver-textdocument'
+
+export default function getPosition(searchString: string, text: string) {
+    const document = TextDocument.create('mockuri://test', 'plaintext', 1, text)
+    const offset = text.indexOf(searchString)
+    const position = document.positionAt(offset)
+    return { line: position.line, character: position.character }
+}
