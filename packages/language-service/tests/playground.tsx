b3c30f3275da849041642dcc9a33b433cda7136c
Aron <i@aron.tw>
2024-03-19 09:20:46 +0800

    Add and delete files and update test files

--- packages/language-service/tests/playground.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default () => (
-    <>
-        <div className='bg:blue fg:rgb(0,0,0)'></div>
-    </>
-)
---
+++ packages/language-service/tests/render-syntax-colors.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default () => (
+    <>
+        <div className='bg:blue fg:rgb(0,0,0) fg:rgb(10,50,30)'></div>
+        <div className='bg:blue-50/.5 b:1|solid|blue-30'></div>
+    </>
+)
