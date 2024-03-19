b4c8938ad80605ee4f0be431aca85318a7d7d526
Aron <i@aron.tw>
2024-03-19 09:20:21 +0800

    Refactor renderSyntaxColors function to improve readability and performance

--- packages/language-service/src/functions/render-syntax-colors.ts
+++ packages/language-service/src/functions/render-syntax-colors.ts
@@ -1,44 +1,55 @@
-import type { Color, ColorInformation } from 'vscode-languageserver-types'
+import type { ColorInformation } from 'vscode-languageserver-types'
 import { instancePattern } from '../utils/regex'
 import MasterCSSLanguageService from '../core'
 import { TextDocument } from 'vscode-languageserver-textdocument'
 import toRGBA from 'color-rgba'
+import { Layer } from '@master/css'
 
-export default async function renderSyntaxColors(this: MasterCSSLanguageService, document: TextDocument): Promise<ColorInformation[]> {
+export default async function renderSyntaxColors(this: MasterCSSLanguageService, document: TextDocument): Promise<ColorInformation[] | undefined> {
     const text = document.getText() ?? ''
     const colorInformations: ColorInformation[] = []
-    let instanceMatch: RegExpExecArray | null
-    while ((instanceMatch = instancePattern.exec(text)) !== null) {
+    const isRGBAValid = (rgba: number[]) => rgba.filter(x => isNaN(x)).length === 0
+
+    for (const instanceMatch of text.matchAll(instancePattern)) {
+        if (instanceMatch.index === undefined) break
         const instanceStartIndex = instanceMatch.index
-        let colorMatch: RegExpExecArray | null
-        let target: string
-        let rgba: number[] | undefined
-        const colorPattern = /(?<=[|:\s"'`]|^)(?:#?[\w-]+(?:-[\d]{1,2})?(?:\/.?[\d]*)?(?:\([^\s)]+\))?(?![:]))/g
-        while ((colorMatch = colorPattern.exec(instanceMatch[0])) !== null) {
-            // replace any | with space
-            target = colorMatch[0].replace(/\|/g, ' ')
-            // convert any color channel string to rgba: [red, green, blue, alpha]
-            rgba = toRGBA(target)
-            // check if rgba is valid
-            if (rgba?.length && rgba.filter(x => isNaN(x)).length === 0) {
-                colorInformations.push({
-                    range: {
-                        start: document.positionAt(instanceStartIndex + colorMatch.index),
-                        end: document.positionAt(instanceStartIndex + colorMatch.index + target.length)
-                    },
-                    color: { red: rgba[0], green: rgba[1], blue: rgba[2], alpha: rgba[3] }
-                })
+        const syntax = instanceMatch[0]
+        const rule = this.css.generate(syntax)[0]
+        if (rule && rule.layer !== Layer.Semantic) {
+            const prefixLenght = rule.prefix?.length || 0
+            let currentTextLength = prefixLenght
+            for (const valueComponent of rule.valueComponents) {
+                if (valueComponent.text === undefined) return  // text is always existing, just for type
+                // TODO: should record valueComponent.token
+                currentTextLength += rule.valueToken?.length || 0
+                // TODO: check number mt:30
+                switch (valueComponent.type) {
+                    default:
+                        // convert any color channel string to rgba: [red, green, blue, alpha]
+                        const rgba = toRGBA(valueComponent.text)
+                        // check if rgba is valid
+                        if (rgba?.length && isRGBAValid(rgba)) {
+                            colorInformations.push({
+                                range: {
+                                    start: document.positionAt(instanceStartIndex + prefixLenght),
+                                    end: document.positionAt(instanceStartIndex + currentTextLength)
+                                },
+                                color: { red: rgba[0] / 255, green: rgba[1] / 255, blue: rgba[2] / 255, alpha: rgba[3] }
+                            })
+                        }
+                        break
+                }
             }
         }
     }
+
     const colorIndexSet = new Set()
-    return colorInformations
-        .filter(item => {
-            if (colorIndexSet.has(item.range.start)) {
-                return false
-            } else {
-                colorIndexSet.add(item.range.start)
-                return true
-            }
-        })
+    return colorInformations.filter(item => {
+        if (colorIndexSet.has(item.range.start)) {
+            return false
+        } else {
+            colorIndexSet.add(item.range.start)
+            return true
+        }
+    })
 }
