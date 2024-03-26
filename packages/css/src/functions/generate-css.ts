23e63c56a7bd7540d3af7bbf5351d1fcc506bc0b
Aron <i@aron.tw>
2024-03-26 15:59:44 +0800

    Perf: Generate CSS rules using the same CSS instance

--- packages/css/src/functions/generate-css.ts
+++ packages/css/src/functions/generate-css.ts
@@ -1,6 +1,16 @@
 import { Config } from '../config'
 import MasterCSS from '../core'
 
-export default function generateCSS(classes: string[], config?: Config) {
-    return new MasterCSS(config).add(...classes).text
+export default function generateCSS(classes: string[], cssOrConfig?: MasterCSS | Config) {
+    // improve performance
+    if (cssOrConfig instanceof MasterCSS) {
+        const css = cssOrConfig
+        let text = ''
+        css.add(...classes)
+        text = css.text
+        css.delete(...classes)
+        return text
+    } else {
+        return new MasterCSS(cssOrConfig).add(...classes).text
+    }
 }
