e46f3296124963c8b8eea6c0f249c7a3c513c6e6
Aron <i@aron.tw>
2025-03-20 14:51:42 +0800

    Refactor external file path resolution in techor.config.ts to use regex pattern

--- packages/language/techor.config.ts
+++ packages/language/techor.config.ts
@@ -1,10 +1,11 @@
 import { Config } from 'techor'
-import glob from 'fast-glob'
 
 export default {
     build: {
         input: {
-            external: glob.sync(new URL('./syntaxes/*.json', import.meta.url).pathname)
+            external: [
+                /syntaxes/
+            ]
         }
     }
 } as Config
