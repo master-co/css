3a23f9f4962496c6df85e54952c3208941c5e5a2
Aron <i@aron.tw>
2025-03-20 14:27:32 +0800

    Update build script and external file path resolution

--- packages/language/techor.config.ts
+++ packages/language/techor.config.ts
@@ -4,7 +4,7 @@ import glob from 'fast-glob'
 export default {
     build: {
         input: {
-            external: glob.sync('./syntaxes/*.json', { absolute: true })
+            external: glob.sync(new URL('./syntaxes/*.json', import.meta.url).pathname)
         }
     }
 } as Config
