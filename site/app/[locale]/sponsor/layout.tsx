62130d0f0bf99e4d4af1ea6fe87093d3a0056485
Aron <i@aron.tw>
2025-03-23 05:36:55 +0800

    Remove unused HTML layout component from sponsor layout

--- site/app/[locale]/sponsor/layout.tsx
+++ site/app/[locale]/sponsor/layout.tsx
@@ -1,7 +1,6 @@
 import Body from 'internal/layouts/body'
 import i18n from 'internal/common/i18n.config.mjs'
 import DocHeader from 'internal/components/DocHeader'
-import HTML from 'internal/layouts/html'
 
 export async function generateStaticParams() {
     return i18n.locales.map((locale: any) => ({ locale }))
@@ -12,11 +11,9 @@ export default async function Layout({ children, locale }: {
     locale: typeof i18n.locales[number]
 }) {
     return (
-        <HTML locale={locale}>
-            <Body className="bg:base">
-                <DocHeader contained />
-                {children}
-            </Body>
-        </HTML>
+        <Body className="bg:base">
+            <DocHeader contained />
+            {children}
+        </Body>
     )
 }
