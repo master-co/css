9356b77c671347e7f1d89f852921606d2def973b
Aron <i@aron.tw>
2025-03-23 00:21:52 +0800

    Docs: Add Master CSS React

---
+++ site/app/[locale]/reference/react/metadata.ts
@@ -0,0 +1,14 @@
+import define from 'internal/utils/metadata'
+
+const metadata = define({
+    title: 'React',
+    description: 'A React integration for Master CSS',
+    category: 'Package',
+    fileURL: import.meta.url,
+    package: {
+        npm: '@master/css.react',
+        source: 'https://github.com/master-co/css/tree/rc/packages/react'
+    }
+})
+
+export default metadata
