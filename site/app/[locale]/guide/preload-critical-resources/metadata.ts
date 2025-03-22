4c31f942aa42fad73cec1ce4295c759d4e48bde3
Aron <i@aron.tw>
2025-03-23 03:25:00 +0800

    Update preload critical resources guide for clarity and optimization

--- site/app/[locale]/guide/preload-critical-resources/metadata.ts
+++ site/app/[locale]/guide/preload-critical-resources/metadata.ts
@@ -1,7 +1,7 @@
 import define from 'internal/utils/metadata'
 
 const metadata = define({
-    title: 'Preloading critical resources',
+    title: 'Optimizing critical resources',
     description: 'The runtime engine plays a crucial role in the initial display of a page.',
     category: 'Production Optimization',
     other: {
