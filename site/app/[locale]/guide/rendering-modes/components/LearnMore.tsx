64d42e8a3b31af11ce8c51765b74efe690b2fb6b
Aron <i@aron.tw>
2025-04-10 03:51:49 +0800

    Fix styles

--- site/app/[locale]/guide/rendering-modes/components/LearnMore.tsx
+++ site/app/[locale]/guide/rendering-modes/components/LearnMore.tsx
@@ -4,8 +4,7 @@ import clsx from 'clsx'
 export default (props: any) => {
     return (
         <Link {...props} className={clsx(`center-content flex r:5 text-decoration:none! my:10 px:4x font:14 font:semibold h:44`, {
-            'b:1 bg:primary fg:text-primary fg:black:hover b:black/.1@light b:white/.2@dark': !props.disabled,
-            'bg:gray-15@dark bg:slate-95@light': props.disabled
+            'b:1 bg:primary fg:text-primary fg:black:hover b:black/.1@light b:white/.2@dark': !props.disabled
         })}>
             {props.children}
         </Link>
