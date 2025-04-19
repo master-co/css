b5f87bcea0573dd6ef722359d49c34909583738b
Aron <i@aron.tw>
2025-04-20 04:57:33 +0800

    Docs: Add Nuxt reference

---
+++ site/app/[locale]/reference/nuxt/metadata.ts
@@ -0,0 +1,16 @@
+import define from 'internal/utils/metadata'
+import pkg from '~/packages/nuxt/package.json'
+
+const metadata = define({
+    title: pkg.name,
+    description: pkg.description,
+    category: 'Package',
+    type: 'entity',
+    fileURL: import.meta.url,
+    package: {
+        npm: pkg.name,
+        source: 'https://github.com/master-co/css/tree/rc/' + pkg.repository.directory
+    }
+})
+
+export default metadata
