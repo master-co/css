cef03fd45068886b50602ba461b498936e9c0c62
Aron <i@aron.tw>
2025-04-21 05:32:49 +0800

    Fix(blog): Adjust padding and margin classes for improved layout

--- site/app/[locale]/blog/page.tsx
+++ site/app/[locale]/blog/page.tsx
@@ -23,7 +23,7 @@ export default async function Page(props: any) {
     return <>
         <main className='px:5x'>
             <HeroHeader metadata={metadata} />
-            <div className="mx:auto py:30x max-w:screen-md prose">
+            <div className="mx:auto max-w:screen-md pt:18x prose pt:30x@sm">
                 <div className='gap:8x grid-cols:1 grid-cols:2@sm'>
                     {pageCategories
                         .map(({ pages }) => pages)
