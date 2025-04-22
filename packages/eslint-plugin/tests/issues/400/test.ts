225b8311b8c87a818dd38ee736f1efc03b639cbb
Aron <i@aron.tw>
2025-04-22 13:08:41 +0800

    Update order test to include valid case and remove invalid cases

--- packages/eslint-plugin/tests/issues/400/test.ts
+++ packages/eslint-plugin/tests/issues/400/test.ts
@@ -2,13 +2,8 @@ import rule from '../../../src/rules/class-validation'
 import { jsxTester, source } from '../../testers'
 
 jsxTester.run('order', rule, {
-    valid: [],
-    invalid: [
-        {
-            ...source('1.input.tsx', import.meta.url),
-            errors: [
-                { messageId: 'invalidClass', line: 4, column: 22, endLine: 4, endColumn: 38 },
-            ]
-        }
-    ]
+    valid: [{
+        ...source('1.input.tsx', import.meta.url),
+    }],
+    invalid: []
 })
