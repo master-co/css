f8456e1b4c749523f866d7cafe158c7fd77cfce8
Aron <i@aron.tw>
2025-04-23 09:21:28 +0800

    Refactor Overview component structure and remove unnecessary whitespace in sitemap route

--- site/app/sitemap.xml/route.ts
+++ site/app/sitemap.xml/route.ts
@@ -1,7 +1,6 @@
 import generateSitemapResponse from 'internal/utils/generate-sitemap-response'
 import pages from '~/site/.pages.json'
 
-
 export function GET(request: Request) {
     return generateSitemapResponse(request, pages)
 }
