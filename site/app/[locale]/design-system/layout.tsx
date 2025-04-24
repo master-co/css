95f07c28b04135f9479780f372d4917788ae2052
Aron <i@aron.tw>
2025-04-24 12:38:35 +0800

    Update subproject reference and add layout component for design system

---
+++ site/app/[locale]/design-system/layout.tsx
@@ -0,0 +1,24 @@
+import Body from 'internal/layouts/body'
+import i18n from 'internal/common/i18n.config.mjs'
+import DocHeader from 'internal/components/DocHeader'
+import DocSidebar from 'internal/components/DocSidebar'
+import DocWrapper from '~/internal/components/DocWrapper'
+
+export async function generateStaticParams() {
+    return i18n.locales.map((locale: any) => ({ locale }))
+}
+
+export default async function Layout({ children }: {
+    children: React.ReactElement,
+    locale: typeof i18n.locales[number]
+}) {
+    return (
+        <Body className="bg:base">
+            <DocHeader contained />
+            <DocWrapper>
+                {children}
+            </DocWrapper>
+        </Body>
+    )
+}
+
--- site/app/[locale]/design-system/page.tsx
+++ site/app/[locale]/design-system/page.tsx
@@ -2,12 +2,10 @@ import createPage from '~/internal/factories/create-page'
 import Layout from 'internal/layouts/doc'
 import metadata from './metadata'
 import dictionaries from '~/site/dictionaries'
-import categories from '~/site/.categories/reference.json'
 
 export const { Page, dynamic, revalidate, generateMetadata } = createPage({
     metadata,
     dictionaries,
-    categories,
     content: import('./content.mdx'),
     Layout,
 })
