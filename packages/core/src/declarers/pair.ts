41815ce5f74415138b0d134c713719de2b1a5202
Aron <i@aron.tw>
2025-05-07 02:29:16 +0800

    Remove unused files and configurations from angular-with-runtime-rendering example

--- packages/core/src/declarers/pair.ts
+++ packages/core/src/declarers/pair.ts
@@ -1,6 +1,7 @@
 import { ValueComponent } from '../types/syntax'
 
-export default function pair(_: string, valueComponents: ValueComponent[], [x, y]: [string, string]) {
+export default function pair(_: string, valueComponents: ValueComponent[], data: [string, string]) {
+    const [x, y] = data
     const length = valueComponents.length
     return {
         [x]: length === 1
