740185a3858fa724abaae47d30a5d1cdb0f101c9
Aron <i@aron.tw>
2025-05-13 17:45:04 +0800

    Refactor Default component: move table structure inside the div for improved layout consistency

--- site/app/[locale]/guide/at/components/Default.tsx
+++ site/app/[locale]/guide/at/components/Default.tsx
@@ -3,29 +3,31 @@ import InlineCode from 'internal/components/InlineCode'
 
 export default () =>
     <div className='doc-table'>
-        <thead>
-            <tr>
-                <th>Token</th>
-                <th>CSS text</th>
-            </tr>
-        </thead>
-        <tbody>
-            {
-                [
-                    ...Object.keys(at)
-                        .map((eachBreakpointName) => {
-                            // @ts-ignore
-                            const eachBreakpoint = at[eachBreakpointName]
-                            return (
-                                <tr key={eachBreakpointName}>
-                                    <th><code>{eachBreakpointName}</code></th>
-                                    <td>
-                                        <InlineCode>{eachBreakpoint}</InlineCode>
-                                    </td>
-                                </tr>
-                            )
-                        })
-                ]
-            }
-        </tbody>
+        <table>
+            <thead>
+                <tr>
+                    <th>Token</th>
+                    <th>CSS text</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    [
+                        ...Object.keys(at)
+                            .map((eachBreakpointName) => {
+                                // @ts-ignore
+                                const eachBreakpoint = at[eachBreakpointName]
+                                return (
+                                    <tr key={eachBreakpointName}>
+                                        <th><code>{eachBreakpointName}</code></th>
+                                        <td>
+                                            <InlineCode>{eachBreakpoint}</InlineCode>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                    ]
+                }
+            </tbody>
+        </table>
     </div>
