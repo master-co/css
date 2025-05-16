221ee3a8f145ab77edc608b79d620f4433f1b14a
Aron <i@aron.tw>
2025-05-16 09:43:11 +0800

    Add: Utilities `clip-none` `clip-margin` `clip-border` `clip-padding` `clip-content` `clip-fill` `clip-stroke` `clip-view`

--- site/app/[locale]/reference/clip-path/syntaxes.ts
+++ site/app/[locale]/reference/clip-path/syntaxes.ts
@@ -1,19 +1,19 @@
 const syntaxes = [
     ['clip-path:`value`'],
+    'clip-none',
+    'clip-margin',
+    'clip-border',
+    'clip-padding',
+    'clip-content',
+    'clip-fill',
+    'clip-stroke',
+    'clip-view',
     ['clip:inset(`…`)'],
     ['clip:circle(`…`)'],
     ['clip:ellipse(`…`)'],
     ['clip:polygon(`…`)'],
     ['clip:path(`…`)'],
     ['clip:url(`…`)'],
-    'clip:none',
-    'clip:margin',
-    'clip:border',
-    'clip:padding',
-    'clip:content',
-    'clip:fill',
-    'clip:stroke',
-    'clip:view',
 ]
 
 export default syntaxes
