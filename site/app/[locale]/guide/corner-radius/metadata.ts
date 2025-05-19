d3968c4dbb38a56cd6aa62ea3de06b2bd7ac1032
Aron <i@aron.tw>
2025-05-19 19:49:05 +0800

    Docs: New document Corner Radius

---
+++ site/app/[locale]/guide/corner-radius/metadata.ts
@@ -0,0 +1,10 @@
+import define from 'internal/utils/metadata'
+
+const metadata = define({
+    title: 'Corner Radius',
+    description: 'Corner radius defines the roundness of UI elements\' corners.',
+    category: 'Fundations',
+    fileURL: import.meta.url
+})
+
+export default metadata
