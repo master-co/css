ac60d15b036443ce3ac62f7af9ee4565d2bc3212
Aron <i@aron.tw>
2026-05-01 19:59:52 +0800

    Update: Toolchain configs

--- packages/nuxt/vitest.config.ts
+++ packages/nuxt/vitest.config.ts
@@ -1,7 +1,10 @@
-import { defineVitestConfig } from '@nuxt/test-utils/config'
+import { defineConfig } from 'vitest/config'
 
-export default defineVitestConfig({
+export default defineConfig({
     test: {
-        environment: 'nuxt'
+        environment: 'node',
+        include: [
+            'test/**/*.test.ts'
+        ]
     }
 })
