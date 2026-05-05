d5b576a73a1dac5e3065fe3967314f86f3bc32f9
Aron <i@aron.tw>
2026-05-06 01:37:46 +0800

    Add Next.js adapter and update installation instructions

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -1,8 +1,9 @@
 import redirects from './redirects.mjs'
 import withCommonNextConfig from 'internal/common/with-next-config.mjs'
+import { withMasterCSS } from '@master/css.next'
 
-const nextConfig = await withCommonNextConfig({
+const nextConfig = withMasterCSS(await withCommonNextConfig({
     redirects
-})
+}))
 
 export default nextConfig
