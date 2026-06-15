8c02b0e746625a9b52f19f44fe4a1ea1bd5ffba5
Aron <i@aron.tw>
2026-06-16 05:32:16 +0800

    Test(Next): Stabilize integration test builds

--- packages/next/vitest.config.ts
+++ packages/next/vitest.config.ts
@@ -1,4 +1,10 @@
 import { defineConfig } from 'vitest/config'
 import config from '../../shared/vitest.config'
 
-export default defineConfig(config)
+export default defineConfig({
+    ...config,
+    test: {
+        ...config.test,
+        fileParallelism: false
+    }
+})
