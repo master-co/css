18346a0c5eb4561899d7fe3c0e06ef2a1256ace1
Aron <i@aron.tw>
2026-06-21 05:11:46 +0800

    Fix(Site): Run lint through ESLint CLI

--- site/eslint.config.mjs
+++ site/eslint.config.mjs
@@ -1,6 +1,11 @@
 import common from 'internal/eslint.config.mjs'
+import { includeIgnoreFile } from '@eslint/compat'
+import { fileURLToPath } from 'node:url'
+
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url))
 
 export default [
+    includeIgnoreFile(gitignorePath),
     ...common,
     // {
     //     rules: {
