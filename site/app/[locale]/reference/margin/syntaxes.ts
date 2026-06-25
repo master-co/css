d1aad4a921a58ad5452c6fa8d20e60a6ef2b3fd7
Aron <i@aron.tw>
2026-06-26 01:44:14 +0800

    Update margin syntaxes to include additional size and direction options for improved flexibility

--- site/app/[locale]/reference/margin/syntaxes.ts
+++ site/app/[locale]/reference/margin/syntaxes.ts
@@ -1,11 +1,20 @@
 const syntaxes = [
+    ['m:`size`'],
+    ['m:`vertical`|`horizontal`'],
+    ['m:`top`|`horizontal`|`bottom`'],
+    ['m:`top`|`right`|`bottom`|`left`'],
     ['mt:`size`'],
+    ['mr:`size`'],
     ['mb:`size`'],
     ['ml:`size`'],
-    ['mr:`size`'],
-    ['m:`size`'],
     ['mx:`size`'],
+    ['mx:`inline-start`|`inline-end`'],
     ['my:`size`'],
+    ['my:`block-start`|`block-end`'],
+    ['mxs:`size`'],
+    ['mxe:`size`'],
+    ['mys:`size`'],
+    ['mye:`size`'],
 ]
 
 export default syntaxes
