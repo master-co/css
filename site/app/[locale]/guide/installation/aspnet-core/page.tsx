ffdf5519328a3dd307c2c4886badbff4795203c0
Aron <i@aron.tw>
2026-07-02 23:07:12 +0800

    Fix(Site): Use brand keys for installation icons

--- site/app/[locale]/guide/installation/aspnet-core/page.tsx
+++ site/app/[locale]/guide/installation/aspnet-core/page.tsx
@@ -9,7 +9,7 @@ export const { Page, dynamic, revalidate, generateMetadata } = createPage({
     dictionaries,
     categories,
     noTOC: true,
-    icon: 'aspnetCore',
+    icon: 'aspnet-core',
     categoryLink: '/guide/installation/integrations',
     content: import('./content.mdx'),
     Layout,
