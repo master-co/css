19872a3382f93822efe2b81678de04a5b1fb4875
Aron <i@aron.tw>
2026-07-03 01:49:09 +0800

    Add Webpack installation and static rendering guides

---
+++ site/app/[locale]/guide/installation/webpack/layout.tsx
@@ -0,0 +1,26 @@
+import createLayout from '~/internal/factories/create-layout'
+import Layout from '~/site/layouts/doc'
+import dictionaries from '~/site/dictionaries'
+import categories from '~/site/.categories/guide.json'
+import Tabs, { Tab, TabBadge } from '~/internal/components/Tabs'
+
+export const { Page, dynamic, revalidate, generateMetadata } = createLayout({
+    metadata: {
+        title: 'Set up Master CSS in Webpack',
+        description: 'Guide to setting up Master CSS in your Webpack project.',
+        category: 'Integrations'
+    },
+    dictionaries,
+    categories,
+    noTOC: true,
+    categoryLink: '/guide/installation/integrations',
+    icon: 'webpack',
+    content: ({ $ }) =>
+        <Tabs className="mb:xl">
+            <Tab href='/guide/installation/webpack'>{$('Runtime Rendering')} <TabBadge>{$('Default')}</TabBadge></Tab>
+            <Tab href='/guide/installation/webpack/static-rendering'>{$('Static Rendering')}</Tab>
+        </Tabs>,
+    Layout,
+})
+
+export default Page
